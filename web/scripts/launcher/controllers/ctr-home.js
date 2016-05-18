2ead39599b98dffca84e681477e07cece0afae89
ezequiel <ezequielc@gmail.com>
2016-05-18 18:08:16 -0300

    Priority Support banner [stage-1]

--- web/scripts/launcher/controllers/ctr-home.js
+++ web/scripts/launcher/controllers/ctr-home.js
@@ -1,15 +1,17 @@
 'use strict';
 
 angular.module('risevision.apps.launcher.controllers')
-  .controller('HomeCtrl', ['$scope', 'launcherTracker', 'editorFactory', 'localStorageService',
+  .controller('HomeCtrl', ['$scope', 'launcherTracker', 'editorFactory',
+    'localStorageService',
     function ($scope, launcherTracker, editorFactory, localStorageService) {
       $scope.launcherTracker = launcherTracker;
       $scope.editorFactory = editorFactory;
-      $scope.showHelp = localStorageService.get("launcher.showHelp") === "true";
+      $scope.showHelp = localStorageService.get('launcher.showHelp') ===
+        'true';
 
-      $scope.toggleHelp = function() {
-      	$scope.showHelp = !$scope.showHelp;
-      	localStorageService.set("launcher.showHelp",$scope.showHelp);
+      $scope.toggleHelp = function () {
+        $scope.showHelp = !$scope.showHelp;
+        localStorageService.set('launcher.showHelp', $scope.showHelp);
       }
     }
   ]); //ctr
