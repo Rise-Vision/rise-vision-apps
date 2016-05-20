8e021b545ba81bf552e7c47edb87804f50d53876
ezequiel <ezequielc@gmail.com>
2016-05-20 13:28:03 -0300

    Add Presentation CTA [stage-2]

--- web/scripts/launcher/controllers/ctr-home.js
+++ web/scripts/launcher/controllers/ctr-home.js
@@ -2,13 +2,20 @@
 
 angular.module('risevision.apps.launcher.controllers')
   .controller('HomeCtrl', ['$scope', 'launcherTracker', 'editorFactory',
-    'localStorageService',
-    function ($scope, launcherTracker, editorFactory, localStorageService) {
+    'localStorageService', '$loading',
+    function ($scope, launcherTracker, editorFactory, localStorageService, $loading) {
       $scope.launcherTracker = launcherTracker;
       $scope.editorFactory = editorFactory;
       $scope.showHelp = localStorageService.get('launcher.showHelp') ===
         'true';
 
+      $loading.startGlobal("launcher.loading");
+      $scope.$watch('editorFactory.presentations.loadingItems',function(loadingItems){
+        if (loadingItems === false) {
+          $loading.stopGlobal("launcher.loading");  
+        }
+      });
+      
       $scope.toggleHelp = function () {
         $scope.showHelp = !$scope.showHelp;
         localStorageService.set('launcher.showHelp', $scope.showHelp);
