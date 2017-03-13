4ee3c2016613366ce51db2a8bb93bbc6adae0cb2
Francisco Vallarino <fjvallarino@gmail.com>
2017-03-13 17:46:15 -0300

    Reload modified files in order to refresh thumbnail

--- web/scripts/storage/controllers/ctr-rename-modal.js
+++ web/scripts/storage/controllers/ctr-rename-modal.js
@@ -1,12 +1,30 @@
 'use strict';
 
 angular.module('risevision.storage.controllers')
-  .controller('RenameModalCtrl', ['$scope', '$modalInstance', '$rootScope', '$translate', 'storage', 'filesFactory', 'sourceName',
-    function ($scope, $modalInstance, $rootScope, $translate, storage, filesFactory, source) {
+  .controller('RenameModalCtrl', ['$scope', '$modalInstance', '$rootScope', '$translate', '$q', 'storage', 'filesFactory', 'sourceName',
+    function ($scope, $modalInstance, $rootScope, $translate, $q, storage, filesFactory, source) {
       $scope.renameName = source.name.replace("/", "");
 
+      function isFile(name) {
+        return name.endsWith("/");
+      }
+
+      function loadSingleFile(file) {
+        if(isFile(file.name)) {
+          return storage.files.get({
+            file: file.name
+          })
+          .then(function (resp) {
+            return resp && resp.files && resp.files[0] ? resp.files[0] : file;
+          });
+        }
+        else {
+          return $q.resolve(file);
+        }
+      }
+
       $scope.ok = function () {
-        var suffix = source.name.endsWith("/") ? "/" : "";
+        var suffix = isFile(source.name) ? "/" : "";
         var renameName = $scope.renameName + suffix;
         var newFile = JSON.parse(JSON.stringify(source));
 
@@ -19,23 +37,31 @@ angular.module('risevision.storage.controllers')
             }
             else {
               console.log('Storage rename processed succesfully');
-
               newFile.name = renameName;
-              filesFactory.addFile(newFile);
-              filesFactory.removeFiles([source]);
-              $modalInstance.close();
+
+              loadSingleFile(newFile)
+                .then(function(file) {
+                  filesFactory.addFile(newFile);
+                  filesFactory.removeFiles([source]);
+                  $modalInstance.close();
+                }, function(e) {
+                  console.log("Error loading files after renaming '" + sourceName + "' to '" + $scope.renameName + "'", e);
+                });
             }
           }, function(e) {
-            console.log("Error renaming '" + sourceName + "' to '" + $scope.renameName + "'");
+            console.log("Error renaming '" + sourceName + "' to '" + $scope.renameName + "'", e);
             $scope.errorKey = "unknown";
           });
       };
+
       $scope.cancel = function () {
         $modalInstance.dismiss('cancel');
       };
+
       $scope.dismiss = function () {
         $modalInstance.dismiss();
       };
+
       $scope.validDestination = function () {
         return $scope.renameName && $scope.renameName.indexOf("/") === -1;
       };
