6810d8827870ffe4266d3607b91c1ea45edaa383
Francisco Vallarino <fjvallarino@gmail.com>
2017-04-12 10:42:48 -0300

    Add static Pending Operations service

---
+++ web/scripts/storage/services/svc-pending-operations.js
@@ -0,0 +1,57 @@
+'use strict';
+angular.module('risevision.storage.services')
+  .factory('pendingOperationsSvc', [
+    function () {
+      var factory = {};
+
+      factory.pendingOperations = [];
+      factory.isPOCollapsed = true;
+
+      factory.addPendingOperation = function (file, action) {
+        console.log("Adding", file, action);
+        if(!findByFileName(file.name)) {
+          console.log("Added!!!", file, action);
+          file.action = action;
+
+          factory.pendingOperations.push(file);
+        }
+      };
+
+      factory.removePendingOperation = function (file) {
+        var existing = findByFileName(file.name);
+        var position = existing ? factory.pendingOperations.indexOf(existing) : -1;
+
+        if (position >= 0) {
+          factory.pendingOperations.splice(position, 1);
+        }
+      };
+
+      factory.removePendingOperations = function(pendingFiles) {
+        // Removed completed pending operations
+        for (var i = factory.pendingOperations.length - 1; i >= 0; i--) {
+          var file = factory.pendingOperations[i];
+
+          if (pendingFiles.indexOf(file) >= 0) {
+            factory.pendingOperations.splice(i, 1);
+          }
+        }
+      };
+
+      factory.getActivePendingOperations = function () {
+        console.log("Getting active blah", factory.pendingOperations);
+        return factory.pendingOperations.filter(function (op) {
+          return !op.actionFailed;
+        });
+      };
+
+      function findByFileName(fileName) {
+        var existing = factory.pendingOperations.filter(function(f) {
+          return f.name === fileName;
+        });
+
+        return existing.length > 0 ? existing[0] : null;
+      }
+
+      return factory;
+    }
+  ]);
