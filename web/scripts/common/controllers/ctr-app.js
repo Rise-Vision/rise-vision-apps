1a42fe956fea852eb621a651da510d40243d141b
Francisco Vallarino <fjvallarino@gmail.com>
2017-10-04 18:41:53 -0300

    Do not load data or show menu if user has not completed registration [stage-3]

--- web/scripts/common/controllers/ctr-app.js
+++ web/scripts/common/controllers/ctr-app.js
@@ -1,60 +1,75 @@
 'use strict';
 
 angular.module('risevision.apps.controllers')
-  .controller('AppCtrl', ['$scope', '$rootScope', '$state', 'STORE_URL',
-    function ($scope, $rootScope, $state, STORE_URL) {
-      $scope.navOptions = [{
-        title: 'Home',
-        link: $state.href('apps.launcher.home'),
-        cid: true,
-        states: [
-          'apps.launcher.home',
-          'apps.launcher.signup',
-          'apps.launcher.signin'
-        ]
-      }, {
-        title: 'Presentations',
-        link: $state.href('apps.editor.home'),
-        cid: true,
-        states: [
-          'apps.editor.home',
-          'apps.editor.list',
-          'apps.editor.workspace.artboard',
-          'apps.editor.workspace.htmleditor'
-        ]
-      }, {
-        title: 'Schedules',
-        link: $state.href('apps.schedules.home'),
-        cid: true,
-        states: [
-          'apps.schedules.home',
-          'apps.schedules.list',
-          'apps.schedules.details',
-          'apps.schedules.add'
-        ]
-      }, {
-        title: 'Displays',
-        link: $state.href('apps.displays.home'),
-        cid: true,
-        states: [
-          'apps.displays.home',
-          'apps.displays.list',
-          'apps.displays.add',
-          'apps.displays.details'
-        ]
-      }, {
-        title: 'Storage',
-        link: $state.href('apps.storage.home'),
-        cid: true,
-        states: ['apps.storage.home']
-      }, {
-        title: 'Store',
-        link: STORE_URL,
-        cid: true,
-        target: '_blank',
-      }];
-      $scope.navSelected = 'apps.launcher.home';
-      $scope.hideCH = false;
+  .controller('AppCtrl', ['$scope', '$rootScope', '$state', 'userState', 'STORE_URL',
+    function ($scope, $rootScope, $state, userState, STORE_URL) {
+      function showAppsMenu(navSelected) {
+        if (userState.isRiseVisionUser()) {
+          $scope.navOptions = [{
+            title: 'Home',
+            link: $state.href('apps.launcher.home'),
+            cid: true,
+            states: [
+              'apps.launcher.home',
+              'apps.launcher.signup',
+              'apps.launcher.signin'
+            ]
+          }, {
+            title: 'Presentations',
+            link: $state.href('apps.editor.home'),
+            cid: true,
+            states: [
+              'apps.editor.home',
+              'apps.editor.list',
+              'apps.editor.workspace.artboard',
+              'apps.editor.workspace.htmleditor'
+            ]
+          }, {
+            title: 'Schedules',
+            link: $state.href('apps.schedules.home'),
+            cid: true,
+            states: [
+              'apps.schedules.home',
+              'apps.schedules.list',
+              'apps.schedules.details',
+              'apps.schedules.add'
+            ]
+          }, {
+            title: 'Displays',
+            link: $state.href('apps.displays.home'),
+            cid: true,
+            states: [
+              'apps.displays.home',
+              'apps.displays.list',
+              'apps.displays.add',
+              'apps.displays.details'
+            ]
+          }, {
+            title: 'Storage',
+            link: $state.href('apps.storage.home'),
+            cid: true,
+            states: ['apps.storage.home']
+          }, {
+            title: 'Store',
+            link: STORE_URL,
+            cid: true,
+            target: '_blank',
+          }];
+          $scope.navSelected = navSelected;
+          $scope.hideCH = false;
+        }
+        else {
+          $scope.navOptions = [];
+          //$scope.navSelected = null;
+          $scope.hideCH = false;
+        }
+      }
+
+      showAppsMenu('apps.launcher.home');
+
+      $rootScope.$on('risevision.profile.refreshed', function() {
+        showAppsMenu($state.current.name);
+      });
 
       $rootScope.$on('$stateChangeSuccess', function () {
         $scope.navSelected = $state.current.name;
--- web/scripts/launcher/controllers/ctr-home.js
+++ web/scripts/launcher/controllers/ctr-home.js
@@ -1,35 +1,45 @@
 'use strict';
 
 angular.module('risevision.apps.launcher.controllers')
-  .controller('HomeCtrl', ['$scope', 'launcherFactory', 'editorFactory',
-    'displayFactory', '$loading',
-    function ($scope, launcherFactory, editorFactory, displayFactory,
-      $loading) {
+  .controller('HomeCtrl', ['$scope', '$rootScope', 'launcherFactory', 'editorFactory',
+    'displayFactory', '$loading', 'userState',
+    function ($scope, $rootScope, launcherFactory, editorFactory, displayFactory,
+      $loading, userState) {
       $scope.launcherFactory = launcherFactory;
       $scope.editorFactory = editorFactory;
       $scope.displayFactory = displayFactory;
 
-      $loading.startGlobal('launcher.loading');
+      function loadItems() {
+        if (userState.isRiseVisionUser()) {
+          $loading.startGlobal('launcher.loading');
 
-      $scope.$watchGroup([
-        'launcherFactory.presentations.loadingItems',
-        'launcherFactory.schedules.loadingItems',
-        'launcherFactory.displays.loadingItems',
-      ], function (newValues) {
-        if (!newValues[0]) {
-          $loading.stopGlobal('launcher.loading');
-          $loading.stop('presentation-list-loader');
-        }
+          $scope.$watchGroup([
+            'launcherFactory.presentations.loadingItems',
+            'launcherFactory.schedules.loadingItems',
+            'launcherFactory.displays.loadingItems',
+          ], function (newValues) {
+            if (!newValues[0]) {
+              $loading.stopGlobal('launcher.loading');
+              $loading.stop('presentation-list-loader');
+            }
 
-        if (!newValues[1]) {
-          $loading.stop('schedules-list-loader');
-        }
+            if (!newValues[1]) {
+              $loading.stop('schedules-list-loader');
+            }
 
-        if (!newValues[2]) {
-          $loading.stop('displays-list-loader');
+            if (!newValues[2]) {
+              $loading.stop('displays-list-loader');
+            }
+          });
+
+          launcherFactory.load();
         }
+      }
+
+      loadItems();
+
+      $rootScope.$on('risevision.profile.refreshed', function() {
+        loadItems();
       });
-
-      launcherFactory.load();
     }
   ]); //ctr
