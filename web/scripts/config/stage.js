2bf7b3de3c54b3e92cfd8aea81216ca410e93a29
Alex Deaconu <alex.deaconu@gmail.com>
2017-10-24 12:19:58 -0400

    Updated dependencies & URLs [stage-3]

--- web/scripts/config/stage.js
+++ web/scripts/config/stage.js
@@ -17,21 +17,21 @@
     .constant('LOCALES_SUFIX', '.json');
 
   angular.module('risevision.common.config')
-    .value('CORE_URL', 'https://registration-reset-emails-2-dot-rvacore-test.appspot.com/_ah/api'); // override default core value
+    .value('CORE_URL', 'https://rvacore-test.appspot.com/_ah/api'); // override default core value
 
   angular.module('risevision.apps.config', [])
     .value('APPS_ENV', 'TEST')
     .value('STORAGE_API_ROOT',
-      'https://custom-auth-dot-storage-dot-rvacore-test.appspot.com/_ah/api')
+      'https://storage-dot-rvacore-test.appspot.com/_ah/api')
     .value('STORE_ENDPOINT_URL',
-      'https://custom-auth-dot-store-dot-rvacore-test.appspot.com/_ah/api') // override default Store server value
-    .value('STORE_SERVER_URL', 'https://custom-auth-dot-store-dot-rvacore-test.appspot.com/')
+      'https://store-dot-rvacore-test.appspot.com/_ah/api') // override default Store server value
+    .value('STORE_SERVER_URL', 'https://store-dot-rvacore-test.appspot.com/')
     .value('RVA_URL', 'http://rva-test.appspot.com')
     .value('VIEWER_URL', 'http://rvaviewer-test.appspot.com')
     .value('ALERTS_WS_URL',
       'https://rvacore-test.appspot.com/alerts/cap')
     .value('STORAGE_ENDPOINT_URL',
-      'https://custom-auth-dot-storage-dot-rvacore-test.appspot.com/_ah/api')
+      'https://storage-dot-rvacore-test.appspot.com/_ah/api')
     .value('MESSAGING_URL',
       'https://display-messaging-staging.risevision.com')
     .value('APPS_URL', '');
