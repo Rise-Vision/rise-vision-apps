3944167187e1094727fabad3531d1ef3ad97d44d
Francisco Vallarino <fjvallarino@gmail.com>
2017-10-25 17:23:07 -0300

    Attempt to fix Sign In/Sign Up e2e issues [stage-3]

--- test/e2e/launcher/cases/signup.js
+++ test/e2e/launcher/cases/signup.js
@@ -3,7 +3,6 @@ var expect = require('rv-common-e2e').expect;
 var SignUpPage = require('./../pages/signUpPage.js');
 var GoogleAuthPage = require('rv-common-e2e').googleAuthPage;
 var CommonHeaderPage = require('rv-common-e2e').commonHeaderPage;
-var SignInPage = require('./../pages/signInPage.js');
 var helper = require('rv-common-e2e').helper;
 
 var SignupScenarios = function() {
@@ -13,20 +12,18 @@ var SignupScenarios = function() {
            "As a user " +
            "I would like to have access to the sign up process", function() {
     this.timeout(2000);// to allow for protactor to load the seperate page
-    var signInPage;
     var signUpPage;
     var googleAuthPage;
     var commonHeaderPage;
     before(function (){
-      signInPage = new SignInPage();
       signUpPage = new SignUpPage();
       googleAuthPage = new GoogleAuthPage();
       commonHeaderPage = new CommonHeaderPage();
     });
 
     it('should not sign in the user through google when it is already signed in',function(){
-      signInPage.get();
-      signInPage.getGoogleLogin();
+      signUpPage.get();
+      signUpPage.getGoogleLogin();
       googleAuthPage.signin();
       helper.waitDisappear(commonHeaderPage.getLoader(), 'CH spinner loader');
       signUpPage.get();
