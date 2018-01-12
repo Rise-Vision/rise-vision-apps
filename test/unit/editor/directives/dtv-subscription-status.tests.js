63f1e0a0719fd091fc6cbee4c84f703f3cc5609e
Francisco Vallarino <fjvallarino@gmail.com>
2018-01-12 17:47:36 -0300

    Update unit tests [stage-1]

--- test/unit/editor/directives/dtv-subscription-status.tests.js
+++ test/unit/editor/directives/dtv-subscription-status.tests.js
@@ -2,7 +2,7 @@
 describe('directive: subscription status', function() {
   beforeEach(module('risevision.editor.services'));
   beforeEach(module('risevision.editor.directives'));
-  var elm, $scope, $compile, item, gadget;
+  var elm, $scope, $compile, item, gadget, EMBEDDED_PRESENTATIONS_CODE;
 
   beforeEach(module(function ($provide) {
     $provide.service('$q', function() {return Q;});
@@ -16,13 +16,19 @@ describe('directive: subscription status', function() {
         }
       };
     });
+    $provide.service('planFactory', function() {
+      return {
+        showPlansModal: sinon.stub()
+      };
+    });
   }));
 
-  beforeEach(inject(function($rootScope, _$compile_, $templateCache) {
+  beforeEach(inject(function($rootScope, $injector, _$compile_, $templateCache) {
+    EMBEDDED_PRESENTATIONS_CODE = $injector.get('EMBEDDED_PRESENTATIONS_CODE');
     item = { objectReference: 'gadgetId' };
     gadget = {
       id: 'gadgetId',
-      subscriptionStatus: 'Subscribed'      
+      subscriptionStatus: 'Subscribed'
     }
     $templateCache.put('partials/editor/subscription-status.html', '<p>{{item.gadget.subscriptionStatus}}</p>');
     $scope = $rootScope.$new();
@@ -37,83 +43,103 @@ describe('directive: subscription status', function() {
 
   it('should compile html', function(done) {
     compileDirective();
-   
+
     setTimeout(function() {
       $scope.$digest();
-      expect(elm.html()).to.equal('<p class="ng-binding">Subscribed</p>');  
+      expect(elm.html()).to.equal('<p class="ng-binding">Subscribed</p>');
       done()
     }, 10);
-    
+
   });
 
   it('should populate scope', function(done) {
     compileDirective();
-   
+
     setTimeout(function() {
       expect(elm.isolateScope().item.gadget).to.equal(gadget);
-      expect(elm.isolateScope().showBuyButton).to.be.false;
+      expect(elm.isolateScope().showSubscribeStoreButton).to.be.false;
+      expect(elm.isolateScope().showSubscribePlanButton).to.be.false;
       expect(elm.isolateScope().showAccountButton).to.be.false;
       expect(elm.isolateScope().className).to.equal('trial');
       done()
     }, 10);
-    
+
   });
 
   describe('Not Subscribed:', function(){
-    it('should show buy button and trial period',function(done){
+    it('should show subscribe button and trial period',function(done){
       gadget.subscriptionStatus = 'Not Subscribed';
       compileDirective();
-   
+
       setTimeout(function() {
         expect(elm.isolateScope().item.gadget).to.equal(gadget);
-        expect(elm.isolateScope().showBuyButton).to.be.true;
+        expect(elm.isolateScope().showSubscribeStoreButton).to.be.true;
+        expect(elm.isolateScope().showSubscribePlanButton).to.be.false;
         expect(elm.isolateScope().showAccountButton).to.be.false;
-        expect(elm.isolateScope().className).to.equal('trial');     
+        expect(elm.isolateScope().className).to.equal('trial');
         done()
       }, 10);
-    })
+    });
+
+    it('should show subscribe button with Plans modal',function(done){
+      gadget.subscriptionStatus = 'Not Subscribed';
+      gadget.productCode = EMBEDDED_PRESENTATIONS_CODE;
+      compileDirective();
+
+      setTimeout(function() {
+        expect(elm.isolateScope().item.gadget).to.equal(gadget);
+        expect(elm.isolateScope().showSubscribeStoreButton).to.be.false;
+        expect(elm.isolateScope().showSubscribePlanButton).to.be.true;
+        expect(elm.isolateScope().showAccountButton).to.be.false;
+        expect(elm.isolateScope().className).to.equal('trial');
+        done()
+      }, 10);
+    });
   });
-  
+
   describe('On Trial:', function(){
-    it('should show buy button and days remaining',function(done){
+    it('should show subscribe button and days remaining',function(done){
       gadget.subscriptionStatus = 'On Trial';
       compileDirective();
-   
+
       setTimeout(function() {
-        expect(elm.isolateScope().item.gadget).to.equal(gadget); 
-        expect(elm.isolateScope().showBuyButton).to.be.true;
+        expect(elm.isolateScope().item.gadget).to.equal(gadget);
+        expect(elm.isolateScope().showSubscribeStoreButton).to.be.true;
+        expect(elm.isolateScope().showSubscribePlanButton).to.be.false;
         expect(elm.isolateScope().showAccountButton).to.be.false;
-        expect(elm.isolateScope().className).to.equal('trial');     
+        expect(elm.isolateScope().className).to.equal('trial');
         done()
       }, 10);
     })
   });
-  
+
   describe('Trial Expired:', function(){
-    it('should show buy button and expired',function(done){
+    it('should show subscribe button and expired',function(done){
       gadget.subscriptionStatus = 'Trial Expired';
       compileDirective();
-   
+
       setTimeout(function() {
-        expect(elm.isolateScope().item.gadget).to.equal(gadget); 
-        expect(elm.isolateScope().showBuyButton).to.be.true;
+        expect(elm.isolateScope().item.gadget).to.equal(gadget);
+        expect(elm.isolateScope().showSubscribeStoreButton).to.be.true;
+        expect(elm.isolateScope().showSubscribePlanButton).to.be.false;
         expect(elm.isolateScope().showAccountButton).to.be.false;
-        expect(elm.isolateScope().className).to.equal('expired');     
+        expect(elm.isolateScope().className).to.equal('expired');
         done()
       }, 10);
     })
   });
 
   describe('Cancelled:', function(){
-    it('should show buy button and days remaining',function(done){
+    it('should show subscribe button and days remaining',function(done){
       gadget.subscriptionStatus = 'Cancelled';
       compileDirective();
-   
+
       setTimeout(function() {
-        expect(elm.isolateScope().item.gadget).to.equal(gadget); 
-        expect(elm.isolateScope().showBuyButton).to.be.true;
+        expect(elm.isolateScope().item.gadget).to.equal(gadget);
+        expect(elm.isolateScope().showSubscribeStoreButton).to.be.true;
+        expect(elm.isolateScope().showSubscribePlanButton).to.be.false;
         expect(elm.isolateScope().showAccountButton).to.be.false;
-        expect(elm.isolateScope().className).to.equal('cancelled');     
+        expect(elm.isolateScope().className).to.equal('cancelled');
         done()
       }, 10);
     })
@@ -123,15 +149,16 @@ describe('directive: subscription status', function() {
     it('should show asccount button',function(done){
       gadget.subscriptionStatus = 'Suspended';
       compileDirective();
-   
+
       setTimeout(function() {
         expect(elm.isolateScope().item.gadget).to.equal(gadget);
-        expect(elm.isolateScope().showBuyButton).to.be.false;
+        expect(elm.isolateScope().showSubscribeStoreButton).to.be.false;
+        expect(elm.isolateScope().showSubscribePlanButton).to.be.false;
         expect(elm.isolateScope().showAccountButton).to.be.true;
-        expect(elm.isolateScope().className).to.equal('suspended');     
+        expect(elm.isolateScope().className).to.equal('suspended');
         done()
       }, 10);
     })
-  }); 
-  
+  });
+
 });
