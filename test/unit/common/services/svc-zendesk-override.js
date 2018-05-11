d2325742e59d838eb609a5be9d5bdcd78f6176b5
Francisco Vallarino <fjvallarino@gmail.com>
2018-05-11 15:40:28 -0300

    Add Zendesk override constant to fix unit tests [stage-1]

---
+++ test/unit/common/services/svc-zendesk-override.js
@@ -0,0 +1,9 @@
+/* jshint maxlen: false */
+
+(function (angular) {
+  'use strict';
+
+  angular.module('risevision.common.support')
+  /* jshint quotmark: single */
+  .value('ZENDESK_WEB_WIDGET_SCRIPT', '');
+})(angular);
