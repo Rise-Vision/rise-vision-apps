49ae597d47fe4a5a1b647d8c5e61eb91befd2a52
Francisco Vallarino <fjvallarino@gmail.com>
2018-05-14 17:05:27 -0300

    Remove unneeded jshint rules [stage-1]

--- test/unit/common/services/svc-zendesk-override.js
+++ test/unit/common/services/svc-zendesk-override.js
@@ -1,9 +1,6 @@
-/* jshint maxlen: false */
-
 (function (angular) {
   'use strict';
 
   angular.module('risevision.common.support')
-  /* jshint quotmark: single */
   .value('ZENDESK_WEB_WIDGET_SCRIPT', '');
 })(angular);
