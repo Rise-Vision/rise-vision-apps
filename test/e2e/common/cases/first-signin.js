8dd805fd612c7351c260b718533577a3b18abf99
Francisco Vallarino <fjvallarino@gmail.com>
2019-01-14 19:48:11 -0300

    Disable First Sign In test suite [stage-1]

--- test/e2e/common/cases/first-signin.js
+++ test/e2e/common/cases/first-signin.js
@@ -16,7 +16,7 @@ var DisplayAddModalPage = require('./../../displays/pages/displayAddModalPage.js
 var FirstSigninScenarios = function() {
 
   browser.driver.manage().window().setSize(1400, 900);
-  describe('First Signin', function () {
+  xdescribe('First Signin', function () {
     var homepage;
     var signInPage;
     var commonHeaderPage;
