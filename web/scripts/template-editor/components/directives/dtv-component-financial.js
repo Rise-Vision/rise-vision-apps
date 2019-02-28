281ec05ad75d7d7637021c9a1be01553228776d9
Francisco Vallarino <fjvallarino@gmail.com>
2019-02-28 01:55:37 -0300

    Add Instrument Selector UI [stage-1]

--- web/scripts/template-editor/components/directives/dtv-component-financial.js
+++ web/scripts/template-editor/components/directives/dtv-component-financial.js
@@ -1,13 +1,56 @@
 'use strict';
 
 angular.module('risevision.template-editor.directives')
-  .directive('templateComponentFinancial', ['templateEditorFactory',
-    function (templateEditorFactory) {
+  .directive('templateComponentFinancial', ['$timeout', 'templateEditorFactory',
+    function ($timeout, templateEditorFactory) {
       return {
         restrict: 'E',
         templateUrl: 'partials/template-editor/components/component-financial.html',
-        link: function ($scope) {
+        link: function ($scope, element) {
           $scope.factory = templateEditorFactory;
+          $scope.showSelector = false;
+
+          $scope.registerDirective({
+            type: 'rise-data-financial',
+            icon: 'fa-line-chart',
+            element: element,
+            onBackHandler: function () {
+              if ($scope.showSelector) {
+                $scope.showSelector = false;
+              }
+              else {
+                $scope.backToList();
+              }
+            }
+          });
+
+          $scope.addInstruments = function () {
+            _startAnimationDelay(0);
+            $scope.showSelector = true;
+          };
+
+          $scope.selectInstruments = function () {
+            _startAnimationDelay(0);
+            $scope.showSelector = false;
+          };
+
+          function _startAnimationDelay(delay) {
+            $scope.animationRunning = true;
+            $timeout(function () {
+              $scope.animationRunning = false;
+            }, !isNaN(delay) ? delay : 500);
+          }
+
+          $scope.instruments = [{
+            name: 'Canadian Dollar',
+            rate: 'CADUSD=X'
+          }, {
+            name: 'Swiss Franc',
+            rate: 'CHFUSD=X'
+          }, {
+            name: 'Hong Kong Dollar',
+            rate: 'HKDUSD=X'
+          }];
         }
       };
     }
