ac92af28f15f2c641da810efa4a8ae5299593020
santiagonoguez <santiago@nul-unu.com>
2019-03-25 13:05:16 -0600

    update status

--- web/scripts/template-editor/components/directives/dtv-component-financial.js
+++ web/scripts/template-editor/components/directives/dtv-component-financial.js
@@ -122,7 +122,7 @@ angular.module('risevision.template-editor.directives')
               }, 1000);
             },
             isHeaderBottomRuleVisible: function() {
-              return $scope.showInstrumentList;
+              return $scope.enteringInstrumentSelector && $scope.showInstrumentList && $scope.exitingInstrumentSelector;
             },
             onBackHandler: function () {
               if ($scope.showSymbolSelector) {
