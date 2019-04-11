640c10328a9b7a49fa92aad7d90111910f04a721
Oleg Khasimkhanov <olegkhasimkhanov@Olegs-MacBook-Pro.local>
2019-04-11 11:29:57 -0400

    Some changes

--- web/scripts/displays/directives/dtv-preconfigured-media-player.js
+++ web/scripts/displays/directives/dtv-preconfigured-media-player.js
@@ -7,19 +7,7 @@ angular.module('risevision.displays.directives')
         restrict: 'E',
         templateUrl: 'partials/displays/preconfigured-media-player.html',
         link: function ($scope) {
-          $scope.factory = displayFactory;
           $scope.display = displayFactory.display;
-
-          $scope.save = function () {
-            if (!$scope.displayAdd.$valid) {
-              console.error('form not valid: ', $scope.displayAdd.errors);
-              return;
-            }
-
-            displayFactory.addDisplay().then(function () {
-              $scope.display = displayFactory.display;
-            });
-          };
 
         } //link()
       };
