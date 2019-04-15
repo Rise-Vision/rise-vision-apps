13432a315fadc287d9e2e90e6ec96d5fdc9961ec
Alex Deaconu <alex.deaconu@risevision.com>
2019-04-15 15:12:25 -0400

    Add spinner to add-display and send-another-email (#904)

--- web/scripts/displays/directives/dtv-send-another-email.js
+++ web/scripts/displays/directives/dtv-send-another-email.js
@@ -1,14 +1,23 @@
 'use strict';
 
 angular.module('risevision.displays.directives')
-  .directive('sendAnotherEmail', ['$filter', 'displayEmail',
-    function ($filter, displayEmail) {
+  .directive('sendAnotherEmail', ['$filter', '$loading', 'displayEmail',
+    function ($filter, $loading, displayEmail) {
       return {
         restrict: 'E',
         templateUrl: 'partials/displays/send-another-email.html',
         scope: true,
         link: function ($scope) {
           $scope.anotherEmail = null;
+          $scope.displayEmail = displayEmail;
+
+          $scope.$watch('displayEmail.sendingEmail', function (loading) {
+            if (loading) {
+              $loading.start('send-another-email');
+            } else {
+              $loading.stop('send-another-email');
+            }
+          });
 
           $scope.sendToAnotherEmail = function () {
             $scope.errorMessage = null;
