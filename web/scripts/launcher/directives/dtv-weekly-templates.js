92547101b04bd8110445395c464e1cb8eb75e2fe
Oleg Khasimkhanov <olegkhasimkhanov@Olegs-MacBook-Pro.local>
2019-04-16 11:49:22 -0400

    Load Templates and handle Click event

--- web/scripts/launcher/directives/dtv-weekly-templates.js
+++ web/scripts/launcher/directives/dtv-weekly-templates.js
@@ -1,12 +1,42 @@
 'use strict';
 
 angular.module('risevision.apps.launcher.directives')
-  .directive('weeklyTemplates', ['$loading',
-    function ($loading) {
+  .directive('weeklyTemplates', ['$loading', 'productsFactory', 'ScrollingListService', 'presentationUtils',
+    'editorFactory', 'templateEditorFactory',
+    function ($loading, productsFactory, ScrollingListService, presentationUtils, editorFactory,
+      templateEditorFactory) {
       return {
         restrict: 'E',
         templateUrl: 'partials/launcher/weekly-templates.html',
-        link: function ($scope) {} //link()
+        link: function ($scope) {
+
+          $scope.search = {
+            // sortBy: 'templateReleaseDate DESC',
+            query: 'templateOfTheWeek:1',
+            category: 'Templates',
+            count: 4
+          };
+
+          $scope.factory = new ScrollingListService(productsFactory.loadProducts,
+            $scope.search);
+
+          $scope.$watch('factory.loadingItems', function (loading) {
+            if (loading) {
+              $loading.start('weekly-templates');
+            } else {
+              $loading.stop('weekly-templates');
+            }
+          });
+
+          $scope.select = function (product) {
+            if (!presentationUtils.isHtmlTemplate(product)) {
+              editorFactory.copyTemplate(product);
+            } else {
+              templateEditorFactory.createFromTemplate(product);
+            }
+          };
+
+        } //link()
       };
     }
   ]);
