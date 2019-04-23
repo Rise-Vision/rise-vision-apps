f52290e20cf7e879e4f3d5adb3b40c246f1813ad
santiagonoguez <santiago@nul-unu.com>
2019-04-23 10:45:33 -0500

    move preview function to preview directive [stage-9]

--- web/scripts/template-editor/directives/dtv-editor-preview-holder.js
+++ web/scripts/template-editor/directives/dtv-editor-preview-holder.js
@@ -1,13 +1,19 @@
 'use strict';
 
 angular.module('risevision.template-editor.directives')
-  .directive('templateEditorPreviewHolder', ['templateEditorFactory',
-    function (templateEditorFactory) {
+  .directive('templateEditorPreviewHolder', ['$sce', 'templateEditorFactory', 'HTML_TEMPLATE_URL',
+    function ($sce, templateEditorFactory, HTML_TEMPLATE_URL) {
       return {
         restrict: 'E',
         templateUrl: 'partials/template-editor/preview-holder.html',
         link: function ($scope) {
           $scope.factory = templateEditorFactory;
+
+          $scope.getEditorPreviewUrl = function(productCode) {
+            var url = HTML_TEMPLATE_URL.replace('PRODUCT_CODE', productCode);
+
+            return $sce.trustAsResourceUrl(url);
+          }
         }
       };
     }
