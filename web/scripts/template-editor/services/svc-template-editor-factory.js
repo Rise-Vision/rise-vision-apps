a9d057e421b1cb245b07495d61b1ad1add278de4
Francisco Vallarino <fjvallarino@gmail.com>
2019-04-23 17:47:15 -0300

    Make messageBox customizable [stage-7]

--- web/scripts/template-editor/services/svc-template-editor-factory.js
+++ web/scripts/template-editor/services/svc-template-editor-factory.js
@@ -243,7 +243,7 @@ angular.module('risevision.template-editor.services')
 
         $log.error(factory.errorMessage, e);
 
-        messageBox(factory.errorMessage, factory.apiError);
+        messageBox(factory.errorMessage, factory.apiError, null, 'template-editor-message-box', 'partials/template-editor/message-box.html');
       };
 
       var _clearMessages = function () {
