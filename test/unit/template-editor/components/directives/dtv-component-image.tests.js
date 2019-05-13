3ef93341dd1932e7883709a39a527cc8bea8666c
santiagonoguez <santiago@nul-unu.com>
2019-05-13 12:20:32 -0500

    test setting image metadata [stage-9]

--- test/unit/template-editor/components/directives/dtv-component-image.tests.js
+++ test/unit/template-editor/components/directives/dtv-component-image.tests.js
@@ -27,6 +27,7 @@ describe('directive: TemplateComponentImage', function() {
 
     $scope.registerDirective = sinon.stub();
     $scope.setAttributeData = sinon.stub();
+    $scope.showNextPanel = sinon.stub();
 
     timeout = $timeout;
     element = $compile("<template-component-image></template-component-image>")($scope);
@@ -45,6 +46,60 @@ describe('directive: TemplateComponentImage', function() {
     expect(directive.icon).to.equal('fa-image');
     expect(directive.show).to.be.a('function');
     expect(directive.onBackHandler).to.be.a('function');
+  });
+
+  it('should set image lists when available as attribute data', function() {
+    var directive = $scope.registerDirective.getCall(0).args[0];
+    var sampleImages = [
+      { "file": "image.png", "thumbnail-url": "http://image" }
+    ];
+
+    $scope.getAttributeData = function() {
+      return sampleImages;
+    }
+
+    directive.show();
+
+    expect($scope.selectedImages).to.deep.equal(sampleImages);
+
+    timeout.flush();
+  });
+
+  it('should get thumbnail URLs when not available as attribute data', function(done) {
+    var directive = $scope.registerDirective.getCall(0).args[0];
+
+    $scope.getAttributeData = function() {
+      return null;
+    }
+    $scope.getBlueprintData = function() {
+      return "file1.png";
+    }
+
+    directive.show();
+    timeout.flush();
+
+    setTimeout(function() {
+      var expectedMetadata = [
+        {
+          "file": "file1.png",
+          "thumbnail-url": "http://lh3.googleusercontent.com/hOkuYaXqdtS2e4fzQGx1zqTFKko71OSDVTrOb84JsOeaUUL8hfOaLaZ5eCquqN20u_NJv_QSwMoNQl-vJ1lT"
+        }
+      ];
+
+      expect($scope.selectedImages).to.deep.equal(expectedMetadata);
+
+      expect($scope.setAttributeData).to.have.been.called.twice;
+
+      expect($scope.setAttributeData.calledWith(
+        "TEST-ID", "metadata", expectedMetadata
+      )).to.be.true;
+
+      expect($scope.setAttributeData.calledWith(
+        "TEST-ID", "files", "file1.png"
+      )).to.be.true;
+
+      done();
+    }, 100);
   });
 
 });
