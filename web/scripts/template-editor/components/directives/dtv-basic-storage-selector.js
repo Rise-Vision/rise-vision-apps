752f86db7e7723c0caba0828cf67e72277c6bc55
Francisco Vallarino <fjvallarino@gmail.com>
2019-05-14 20:19:56 -0300

    Add basic navigation into folders [stage-7]

--- web/scripts/template-editor/components/directives/dtv-basic-storage-selector.js
+++ web/scripts/template-editor/components/directives/dtv-basic-storage-selector.js
@@ -11,8 +11,15 @@ angular.module('risevision.template-editor.directives')
         },
         templateUrl: 'partials/template-editor/common/basic-storage-selector.html',
         link: function ($scope) {
-          $scope.folderItems = [];
+          $scope.folderItems = [{
+            name: 'test1.jpg'
+          }, {
+            name: 'test2.jpg'
+          }, {
+            name: 'test3.jpg'
+          }];
           $scope.storageUploadManager = {
+            folderPath: '',
             onUploadStatus: function (isUploading) {
               $scope.isUploading = isUploading;
             },
@@ -20,6 +27,31 @@ angular.module('risevision.template-editor.directives')
               console.log('Added file to uploadManager', file);
             }
           };
+
+          $scope.isFolder = function (path) {
+            return path[path.length - 1] === '/';
+          };
+
+          $scope.fileNameOf = function (path) {
+            var parts = path.split('/');
+
+            if ($scope.isFolder(path)) {
+              return parts[parts.length - 2] + '/';
+            }
+            else {
+              return parts.pop();
+            }
+          };
+
+          $scope.loadItems = function (folderPath) {
+            storage.files.get({ folderPath: folderPath }).then(function (items) {
+              $scope.folderItems = items.files.filter(function (item) {
+                return item.name !== folderPath;
+              });
+            });
+          };
+
+          $scope.loadItems();
         }
       };
     }
--- web/scripts/template-editor/components/directives/dtv-component-image.js
+++ web/scripts/template-editor/components/directives/dtv-component-image.js
@@ -125,7 +125,7 @@ angular.module('risevision.template-editor.directives')
 
           $scope.fileNameOf = function( path ) {
             return path.split('/').pop();
-          }
+          };
 
           $scope.uploadImages = function () {
             $scope.showNextPanel('.upload-images-container');
