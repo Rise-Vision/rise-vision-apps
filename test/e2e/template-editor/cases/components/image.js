2788703e3c37f31c6036fab3bdddf54f3c0a5c9c
Francisco Vallarino <fjvallarino@gmail.com>
2019-06-06 00:33:34 -0300

    Add Image component Storage Selector e2e tests [stage-7]

--- test/e2e/template-editor/cases/components/image.js
+++ test/e2e/template-editor/cases/components/image.js
@@ -36,14 +36,14 @@ var ImageComponentScenarios = function () {
 
     describe('upload', function () {
       it('should upload a file and show the corresponding upload panel', function () {
-        var uploadFilePath = process.cwd() + '/web/images/e2e-upload-image.png';
+        var uploadFilePath = process.cwd() + '/web/images/e2e-upload-image-1.png';
         imageComponentPage.getUploadInputMain().sendKeys(uploadFilePath);
 
         expect(imageComponentPage.getUploadPanelMain().isDisplayed()).to.eventually.be.true;
       });
 
       it('should hide upload panel when finished', function() {
-        helper.waitDisappear(imageComponentPage.getUploadPanelMain(), 'Storage Upload Panel');
+        helper.waitDisappear(imageComponentPage.getUploadPanelMain(), 'Main Upload Panel');
         expect(imageComponentPage.getUploadPanelMain().isDisplayed()).to.eventually.be.false;
       });
 
@@ -52,12 +52,51 @@ var ImageComponentScenarios = function () {
       });
     });
 
+    describe('storage', function () {
+      describe('basic operations', function () {
+        it('should load Storage page', function () {
+          helper.wait(imageComponentPage.getStorageButtonMain(), 'Storage Button Main');
+          helper.clickWhenClickable(imageComponentPage.getStorageButtonMain(), 'Storage Button Main');
+          browser.sleep(500);
+          helper.waitDisappear(imageComponentPage.getStorageSpinner(), 'Storage Spinner');
+          expect(imageComponentPage.getStorageItems().count()).to.eventually.equal(1);
+        });
+      });
+
+      describe('upload', function () {
+        it('should upload a file and show the corresponding upload panel', function () {
+          var uploadFilePath = process.cwd() + '/web/images/e2e-upload-image-2.png';
+          imageComponentPage.getUploadInputStorage().sendKeys(uploadFilePath);
+
+          expect(imageComponentPage.getUploadPanelStorage().isDisplayed()).to.eventually.be.true;
+        });
+
+        it('should hide upload panel when finished', function() {
+          helper.waitDisappear(imageComponentPage.getUploadPanelStorage(), 'Storage Upload Panel');
+          expect(imageComponentPage.getUploadPanelStorage().isDisplayed()).to.eventually.be.false;
+        });
+
+        it('should list uploaded file only, with sample files removed', function() {
+          expect(imageComponentPage.getStorageItems().count()).to.eventually.equal(2);
+        });
+
+        it('should select one file and add it to Image List', function() {
+          helper.clickWhenClickable(imageComponentPage.getStorageNewFile(), 'Storage New File');
+          browser.sleep(500);
+          helper.clickWhenClickable(imageComponentPage.getStorageAddSelected(), 'Storage Add Selected');
+
+          helper.wait(imageComponentPage.getListDurationComponent(), 'List Duration');
+          expect(imageComponentPage.getSelectedImagesMain().count()).to.eventually.equal(2);
+        });
+      });
+    });
+
     describe('save and validations', function () {
       it('should save the Presentation, reload it, and validate changes were saved', function () {
         presentationsListPage.changePresentationName(presentationName);
         presentationsListPage.savePresentation();
 
-        //log presentaion / company URL for troubleshooting
+        //log presentation / company URL for troubleshooting
         browser.getCurrentUrl().then(function(actualUrl) {
           console.log(actualUrl);
         });
@@ -68,7 +107,7 @@ var ImageComponentScenarios = function () {
         presentationsListPage.loadPresentation(presentationName);
         templateEditorPage.selectComponent('Image - ');
 
-        expect(imageComponentPage.getSelectedImagesMain().count()).to.eventually.equal(1);
+        expect(imageComponentPage.getSelectedImagesMain().count()).to.eventually.equal(2);
       });
     });
   });
--- test/e2e/template-editor/pages/components/imageComponentPage.js
+++ test/e2e/template-editor/pages/components/imageComponentPage.js
@@ -6,6 +6,15 @@ var ImageComponentPage = function() {
   var uploadButtonMain = element(by.id('image-list-uploader-label'));
   var uploadInputMain = element(by.id('image-list-uploader'));
   var uploadPanelMain = element(by.id('upload-panel-image-list-uploader'));
+  var storageSpinner = element(by.id('storage-image-storage-spinner'));
+  var storageItemsSelector = 'item in folderItems track by $index';
+  var storageItems = element.all(by.repeater(storageItemsSelector));
+  var storageButtonMain = element(by.id('image-list-storage-button'));
+  var storageAddSelected = element(by.id('image-storage-add-selected'));
+  var storageNewFile = element(by.repeater(storageItemsSelector).row(1));
+  var uploadButtonStorage = element(by.id('image-storage-uploader-label'));
+  var uploadInputStorage = element(by.id('image-storage-uploader'));
+  var uploadPanelStorage = element(by.id('upload-panel-image-storage-uploader'));
 
   this.getSelectedImagesMain = function () {
     return selectedImagesMain;
@@ -25,6 +34,38 @@ var ImageComponentPage = function() {
 
   this.getUploadPanelMain = function () {
     return uploadPanelMain;
+  };
+
+  this.getStorageSpinner = function () {
+    return storageSpinner;
+  };
+
+  this.getStorageItems = function () {
+    return storageItems;
+  };
+
+  this.getStorageButtonMain = function () {
+    return storageButtonMain;
+  };
+
+  this.getStorageAddSelected = function () {
+    return storageAddSelected;
+  };
+
+  this.getStorageNewFile = function () {
+    return storageNewFile;
+  };
+
+  this.getUploadButtonStorage = function () {
+    return uploadButtonStorage;
+  };
+
+  this.getUploadInputStorage = function () {
+    return uploadInputStorage;
+  };
+
+  this.getUploadPanelStorage = function () {
+    return uploadPanelStorage;
   };
 };
 
