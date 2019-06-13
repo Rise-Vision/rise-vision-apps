6e60cef9b3a02c569f215f456a23f54aad75a832
Ezequiel Conte <ezequielc@gmail.com>
2019-06-13 15:53:44 -0300

    Improve E2E: wait for Presentation Settings to reveal [stage-1]

--- test/e2e/editor/cases/presentation-add.js
+++ test/e2e/editor/cases/presentation-add.js
@@ -126,6 +126,7 @@ var PresentationAddScenarios = function() {
     it('should delete presentation and return to list', function() {
       workspacePage.getPresentationPropertiesButton().click();
       helper.wait(presentationPropertiesModalPage.getPresentationPropertiesModal(), 'Presentation Properties Modal');
+      browser.sleep(500);
       helper.clickWhenClickable(presentationPropertiesModalPage.getDeleteButton(), 'Presentation Delete Button');
 
       browser.sleep(500);
