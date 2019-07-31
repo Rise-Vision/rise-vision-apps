01176fb5a0e0aa63e3c0ebf68703dd5586aadcec
santiagonoguez <santiago@nul-unu.com>
2019-07-31 14:17:08 -0500

    add partial implementation of existence check [stage-10]

--- web/scripts/template-editor/components/services/svc-file-existence-check.js
+++ web/scripts/template-editor/components/services/svc-file-existence-check.js
@@ -5,10 +5,44 @@ angular.module('risevision.template-editor.services')
     function ($q, $log) {
       var service = {};
 
-      service.requestMetadataFor = function (files, defaultThumbnailUrl) {
-        console.log(files);
+      function _loadMetadata(metadata, fileNames) {
+        var promises = _.map(fileNames, function (fileName) {
+          return _getThumbnailDataFor(fileName)
+            .then(function (data) {
+              return {
+                file: fileName,
+                exists: data.exists,
+                'time-created': data.timeCreated,
+                'thumbnail-url': data.url
+              };
+            })
+            .catch(function (error) {
+              $log.error(error);
+            });
+        });
 
-        return $q.resolve([]);
+        return $q.all(promises).then(function (results) {
+          var metadata = [];
+
+          _.reject(results, _.isNil).forEach(function (file) {
+            metadata.push(file);
+          });
+
+          return metadata;
+        });
+      }
+
+      service.requestMetadataFor = function (files, defaultThumbnailUrl) {
+        var fileNames;
+
+        if (files) {
+          fileNames = Array.isArray(files) ?
+            angular.copy(files) : files.split('|');
+        } else {
+          fileNames = [];
+        }
+
+        return _loadMetadata(fileNames);
       };
 
       return service;
