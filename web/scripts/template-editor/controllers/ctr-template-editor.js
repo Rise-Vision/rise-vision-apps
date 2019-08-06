6fe757817fdb3f784ca4098ffbfd0afa50296beb
santiagonoguez <santiago@nul-unu.com>
2019-08-06 14:43:38 -0500

    debug loading flag [stage-8]

--- web/scripts/template-editor/controllers/ctr-template-editor.js
+++ web/scripts/template-editor/controllers/ctr-template-editor.js
@@ -236,6 +236,7 @@ angular.module('risevision.template-editor.controllers')
       });
 
       $scope.$watch('factory.loadingPresentation', function (loading) {
+        console.log('loading', loading);
         if (loading) {
           $loading.start('template-editor-loader');
         } else {
