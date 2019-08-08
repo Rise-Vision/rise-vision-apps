c4683f3e922de8293e1300ceb55304fc44f62e61
santiagonoguez <santiago@nul-unu.com>
2019-08-08 13:06:13 -0500

    remove debug [stage-9]

--- web/scripts/template-editor/controllers/ctr-template-editor.js
+++ web/scripts/template-editor/controllers/ctr-template-editor.js
@@ -173,7 +173,6 @@ angular.module('risevision.template-editor.controllers')
         ];
 
         if (!newValue.id) {
-          console.log('no id, saving presentation', newValue);
           $scope.factory.save();
           return;
         }
