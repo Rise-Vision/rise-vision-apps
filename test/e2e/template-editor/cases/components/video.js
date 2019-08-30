2b57709b112fe0d32683c7d601763dfe6a92abdb
Will Read <will@willread.ca>
2019-08-30 12:16:39 -0600

    Add e2e tests for video [stage-10]

--- test/e2e/template-editor/cases/components/video.js
+++ test/e2e/template-editor/cases/components/video.js
@@ -10,6 +10,14 @@ var VideoComponentScenarios = function () {
 
   browser.driver.manage().window().setSize(1920, 1080);
 
+  var getUserAgent = function () {
+    return browser.driver.executeScript('return navigator.userAgent');
+  };
+
+  var setUserAgent = function (userAgent) {
+    return browser.driver.executeScript('Object.defineProperty(navigator, "userAgent", {get: function () { return "' + userAgent + '"; }});');
+  };
+
   describe('Video Component', function () {
     var testStartTime = Date.now();
     var presentationName = 'Video Component Presentation - ' + testStartTime;
@@ -60,6 +68,22 @@ var VideoComponentScenarios = function () {
       it('should auto-save the Presentation after updating the video list', function () {
         helper.wait(templateEditorPage.getSavingText(), 'Video component auto-saving');
         helper.wait(templateEditorPage.getSavedText(), 'Video component auto-saved');
+      });
+
+      it('should use a specific accept attribute value when not on a mobile device', function (done) {
+        expect(videoComponentPage.getUploadInputMain()).to.equal('.mp4, .webm');
+      });
+
+      it('should use a generic accept attribute value when on a mobile device', function (done) {
+        var initialUserAgent = getUserAgent();
+
+        setUserAgent('Mozilla/5.0 (Linux; Android 6.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/76.0.3809.100 Mobile Safari/537.36').then(function () {
+          expect(videoComponentPage.getUploadInputMain()).to.equal('video/*');
+
+          setUserAgent(initialUserAgent).then(function () {
+            done();
+          });
+        });
       });
     });
 
