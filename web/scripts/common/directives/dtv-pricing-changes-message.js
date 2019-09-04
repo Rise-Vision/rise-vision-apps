3e0b18cc6e8e3b446912071d90e27b325c5c2aaa
Alex Deaconu <alex.deaconu@gmail.com>
2019-09-04 13:43:13 -0400

    gulp pretty updates [stage-19]

--- web/scripts/common/directives/dtv-pricing-changes-message.js
+++ web/scripts/common/directives/dtv-pricing-changes-message.js
@@ -2,35 +2,37 @@
 
 angular.module('risevision.apps.directives')
   .directive('pricingChangesMessage', ['localStorageService', 'userState',
-  function (localStorageService, userState) {
-    return {
-      restrict: 'E',
-      scope: {},
-      templateUrl: 'partials/common/pricing-changes-message.html',
-      link: function ($scope) {
-        var alertDismissedKey = 'pricingChangesAlert.dismissed';
-        var alertDismissed = localStorageService.get(alertDismissedKey) === 'true';
-        var isPastCreationDate = false;
+    function (localStorageService, userState) {
+      return {
+        restrict: 'E',
+        scope: {},
+        templateUrl: 'partials/common/pricing-changes-message.html',
+        link: function ($scope) {
+          var alertDismissedKey = 'pricingChangesAlert.dismissed';
+          var alertDismissed = localStorageService.get(alertDismissedKey) === 'true';
+          var isPastCreationDate = false;
 
-        var _checkCreationDate = function() {
-          var company = userState.getCopyOfSelectedCompany();
-          var creationDate = ((company && company.creationDate) ? (new Date(company.creationDate)) : (new Date()));
+          var _checkCreationDate = function () {
+            var company = userState.getCopyOfSelectedCompany();
+            var creationDate = ((company && company.creationDate) ? (new Date(company.creationDate)) : (
+            new Date()));
 
-          isPastCreationDate = creationDate < new Date('June 25, 2019');
-        };
+            isPastCreationDate = creationDate < new Date('June 25, 2019');
+          };
 
-        _checkCreationDate();
+          _checkCreationDate();
 
-        $scope.$on('risevision.company.selectedCompanyChanged', _checkCreationDate);
+          $scope.$on('risevision.company.selectedCompanyChanged', _checkCreationDate);
 
-        $scope.alertVisible = function() {
-          return !alertDismissed && isPastCreationDate;
-        };
+          $scope.alertVisible = function () {
+            return !alertDismissed && isPastCreationDate;
+          };
 
-        $scope.dismissAlert = function () {
-          alertDismissed = true;
-          localStorageService.set(alertDismissedKey, 'true');
-        };
-      }
-    };
-  }]);
+          $scope.dismissAlert = function () {
+            alertDismissed = true;
+            localStorageService.set(alertDismissedKey, 'true');
+          };
+        }
+      };
+    }
+  ]);
