2975d1f5bfd626528f74e6e162b3f22e4248d46d
Alex Deaconu <alex.deaconu@gmail.com>
2019-09-04 13:40:25 -0400

    Check companyCreation date for pricing changes message
    Do not show message if Company was created after Jun 25, 2019
    
    [stage-19]

--- web/scripts/common/directives/dtv-pricing-changes-message.js
+++ web/scripts/common/directives/dtv-pricing-changes-message.js
@@ -1,17 +1,34 @@
 'use strict';
 
 angular.module('risevision.apps.directives')
-  .directive('pricingChangesMessage', ['localStorageService', function (localStorageService) {
+  .directive('pricingChangesMessage', ['localStorageService', 'userState',
+  function (localStorageService, userState) {
     return {
       restrict: 'E',
+      scope: {},
       templateUrl: 'partials/common/pricing-changes-message.html',
       link: function ($scope) {
         var alertDismissedKey = 'pricingChangesAlert.dismissed';
+        var alertDismissed = localStorageService.get(alertDismissedKey) === 'true';
+        var isPastCreationDate = false;
 
-        $scope.alertVisible = localStorageService.get(alertDismissedKey) !== 'true';
+        var _checkCreationDate = function() {
+          var company = userState.getCopyOfSelectedCompany();
+          var creationDate = ((company && company.creationDate) ? (new Date(company.creationDate)) : (new Date()));
+
+          isPastCreationDate = creationDate < new Date('June 25, 2019');
+        };
+
+        _checkCreationDate();
+
+        $scope.$on('risevision.company.selectedCompanyChanged', _checkCreationDate);
+
+        $scope.alertVisible = function() {
+          return !alertDismissed && isPastCreationDate;
+        };
 
         $scope.dismissAlert = function () {
-          $scope.alertVisible = false;
+          alertDismissed = true;
           localStorageService.set(alertDismissedKey, 'true');
         };
       }
