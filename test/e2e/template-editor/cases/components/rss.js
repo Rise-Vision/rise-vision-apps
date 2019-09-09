c29ad2d9d92bc6da54d8aa25804ff341fcfe8103
Francisco Vallarino <fjvallarino@gmail.com>
2019-09-09 13:05:20 -0300

    Use new default value [stage-7]

--- test/e2e/template-editor/cases/components/rss.js
+++ test/e2e/template-editor/cases/components/rss.js
@@ -35,7 +35,7 @@ var RssComponentScenarios = function () {
         expect(rssComponentPage.getRssFeedInput().isEnabled()).to.eventually.be.true;
         expect(rssComponentPage.getRssFeedInput().getAttribute('value')).to.eventually.equal('https://www.feedforall.com/sample.xml');
         expect(rssComponentPage.getRssMaxItemsSelect().isEnabled()).to.eventually.be.true;
-        expect(rssComponentPage.getRssMaxItemsSelect().getAttribute('value')).to.eventually.equal('15');
+        expect(rssComponentPage.getRssMaxItemsSelect().getAttribute('value')).to.eventually.equal('1');
       });
 
       function _testInvalidScenario (feedUrl, expectedMessageKey) {
