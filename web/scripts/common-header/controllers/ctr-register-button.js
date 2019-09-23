e801bbc2806520da712560a784180b926cfafc10
Alex Deaconu <alex.deaconu@gmail.com>
2019-09-23 14:02:20 -0400

    Add common-header and component files to Apps
    Update index.html with new paths
    Update partials to point to new paths
    
    [stage-2]

---
+++ web/scripts/common-header/controllers/ctr-register-button.js
@@ -0,0 +1,10 @@
+angular.module("risevision.common.header")
+  .controller("RegisterButtonCtrl", ["$scope", "$cookies", "uiFlowManager",
+    function ($scope, $cookies, uiFlowManager) {
+
+      $scope.register = function () {
+        $cookies.remove("surpressRegistration");
+        uiFlowManager.invalidateStatus("registrationComplete");
+      };
+    }
+  ]);
