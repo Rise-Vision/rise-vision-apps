eb9b5278a766e612d9a3b717d7720171633fdd42
Alex Deaconu <alex.deaconu@gmail.com>
2019-10-04 20:47:42 -0400

    Move first schedule and autosave tests to template-add
    Refactor other template editor cases
    Remove editor3
    
    [stage-16]

--- test/e2e/template-editor/cases/template-editor-add.js
+++ test/e2e/template-editor/cases/template-editor-add.js
@@ -2,6 +2,7 @@
 var expect = require('rv-common-e2e').expect;
 var PresentationListPage = require('./../pages/presentationListPage.js');
 var TemplateEditorPage = require('./../pages/templateEditorPage.js');
+var AutoScheduleModalPage = require('./../../schedules/pages/autoScheduleModalPage.js');
 var helper = require('rv-common-e2e').helper;
 
 var TemplateAddScenarios = function() {
@@ -13,10 +14,12 @@ var TemplateAddScenarios = function() {
     var presentationName = 'Example Presentation - ' + testStartTime;
     var presentationsListPage;
     var templateEditorPage;
+    var autoScheduleModalPage;
 
     before(function () {
       presentationsListPage = new PresentationListPage();
       templateEditorPage = new TemplateEditorPage();
+      autoScheduleModalPage = new AutoScheduleModalPage();
 
       presentationsListPage.loadCurrentCompanyPresentationList();
       presentationsListPage.createNewPresentationFromTemplate('Example Financial Template V4', 'example-financial-template-v4');
@@ -29,6 +32,29 @@ var TemplateAddScenarios = function() {
         helper.waitDisappear(templateEditorPage.getDirtyText());
         helper.waitDisappear(templateEditorPage.getSavingText());
         helper.wait(templateEditorPage.getSavedText(), 'Component auto-saved');
+      });
+
+      it('should set presentation name', function(done) {
+        templateEditorPage.getPresentationName().getAttribute('value').then(function(name) {
+          expect(name).to.contain('Copy of');
+
+          done();
+        });
+      });
+
+      it('should auto create Schedule when publishing Presentation', function () {
+        helper.clickWhenClickable(templateEditorPage.getPublishButton(), 'Publish Button');
+
+        browser.sleep(500);
+
+        helper.wait(autoScheduleModalPage.getAutoScheduleModal(), 'Auto Schedule Modal');
+
+        expect(autoScheduleModalPage.getAutoScheduleModal().isDisplayed()).to.eventually.be.true;
+
+        helper.clickWhenClickable(autoScheduleModalPage.getCloseButton(), 'Auto Schedule Modal - Close Button');
+
+        helper.waitDisappear(autoScheduleModalPage.getAutoScheduleModal(), 'Auto Schedule Modal');
+        helper.waitDisappear(presentationsListPage.getTemplateEditorLoader());
       });
 
       it('should show more than one component', function () {
@@ -46,19 +72,18 @@ var TemplateAddScenarios = function() {
         helper.wait(templateEditorPage.getSavedText(), 'Component auto-saved');
       });
 
-      it('should publish the Presentation', function () {
-        helper.clickWhenClickable(templateEditorPage.getPublishButton(), 'Publish Button');
-
-        helper.wait(templateEditorPage.getSavingText(), 'Component auto-saving');
-        helper.wait(templateEditorPage.getSavedText(), 'Component auto-saved');
-      });
-
       it('should load the newly created Presentation', function () {
         presentationsListPage.loadPresentation(presentationName);
 
         expect(templateEditorPage.getComponentItems().count()).to.eventually.be.above(1);
         expect(templateEditorPage.getImageComponentEdit().isPresent()).to.eventually.be.true;
       });
+
+      it('should not have auto-published the Presentation when navigating', function () {
+        // prevents reoccurrence of issue 1186
+        expect(templateEditorPage.getPublishButton().isEnabled()).to.eventually.be.true;
+      });
+
     });
   });
 };
