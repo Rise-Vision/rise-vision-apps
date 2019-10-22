e54d7d0889c0e86f0da15fe957be575ead744815
Francisco Vallarino <fjvallarino@gmail.com>
2019-10-22 19:21:32 -0300

    First attempts at getting date component styled correctly. Also some temporary ugly hacks [stage-7]

--- web/scripts/template-editor/components/directives/dtv-component-counter.js
+++ web/scripts/template-editor/components/directives/dtv-component-counter.js
@@ -7,23 +7,73 @@ angular.module('risevision.template-editor.directives')
         restrict: 'E',
         scope: true,
         templateUrl: 'partials/template-editor/components/component-counter.html',
-        link: function ($scope, element) {
-          $scope.factory = templateEditorFactory;
+        compile: function () {
+          return {
+            pre: function($scope) {
+              $scope.dateOptions = {
+                formatYear: 'yy',
+                startingDay: 1,
+                showWeeks: false,
+                showButtonBar: false
+              };
 
-          $scope.registerDirective({
-            type: 'rise-data-counter',
-            iconType: 'streamline',
-            icon: 'hourglass',
-            element: element,
-            show: function () {
-              element.show();
-              $scope.componentId = $scope.factory.selected.id;
+              $scope.datePickers = {
+                downToDate: {
+                  isOpen: false,
+                  initDate: null
+                }
+              };
             },
-            getTitle: function (component) {
-              return 'template.rise-data-counter' + '-' + component.attributes.type.value;
-            }
-          });
+            post: function ($scope, element) {
+              $scope.factory = templateEditorFactory;
 
+              $scope.registerDirective({
+                type: 'rise-data-counter',
+                iconType: 'streamline',
+                icon: 'hourglass',
+                element: element,
+                show: function () {
+                  $scope.componentId = $scope.factory.selected.id;
+                  _load();
+                },
+                getTitle: function (component) {
+                  return 'template.rise-data-counter' + '-' + component.attributes.type.value;
+                }
+              });
+
+              function _load () {
+                $scope.date = $scope.getAvailableAttributeData($scope.componentId, 'date');
+                $scope.time = $scope.getAvailableAttributeData($scope.componentId, 'time');
+
+                $scope.countDownTo = $scope.time ? 'downToTime' : 'downToDate';
+              }
+
+              $scope.openSystemDatePicker = function () {
+                let date = document.querySelector('#nativeDatePicker');
+
+                date.style.display = 'block';
+                date.focus();
+                date.click();
+              };
+
+              $scope.closeSystemDatePicker = function () {
+                let date = document.querySelector('#nativeDatePicker');
+
+                date.style.display = 'none';
+              };
+
+              $scope.save = function () {
+                //$scope.setAttributeData($scope.componentId, 'date', $scope.date);
+              };
+
+              $scope.openDatepicker = function ($event, which) {
+                $event.preventDefault();
+                $event.stopPropagation();
+
+                $scope.datePickers[which].isOpen = !$scope.datePickers[which].isOpen;
+              };
+            }
+          };
         }
       };
     }
