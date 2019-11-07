b32155a69d538cfe267c9506bfd2456614a1ad02
Ezequiel Conte <ezequielc@gmail.com>
2019-11-07 17:53:29 -0300

    watch form field [stage-1]

--- web/scripts/components/password-meter/dtv-password-meter.js
+++ web/scripts/components/password-meter/dtv-password-meter.js
@@ -8,51 +8,38 @@
       function ($templateCache, $ocLazyLoad) {
         return {
           restrict: 'E',
-          require: '?ngModel',
           scope: {
-            ngModel: '='
+            watchValue: '='
           },
           template: $templateCache.get(
             'partials/components/password-meter/password-meter.html'),
           link: function (scope, element, attrs, ctrl) {
-            // scope.ngModelCtrl = ctrl;
-
             var _initZxcvbn = function() {
               return $ocLazyLoad.load('vendor/zxcvbn/zxcvbn.js');
             };
-
             _initZxcvbn();
 
-            scope.$watch('ngModel', function (newValue, oldValue) {
-              if (newValue !== oldValue) {
-                // scope.ngModelCtrl.$setDirty(true);
-                _initZxcvbn().then(function(){
-                 scope.result = zxcvbn(newValue);
-                 scope.scorePercentage = Math.max(25,scope.result.score/4 * 100);
-                 _updateStrength(scope.result.score);
-                });
-              }
+            scope.$watch('watchValue', function (newValue, oldValue) {
+              _initZxcvbn().then(function(){
+               var result = zxcvbn(newValue);
+               scope.scorePercentage = Math.max(25, result.score/4 * 100);
+               scope.feedback = result.feedback.warning;
+               _updateStrength(result.score);
+              });
             });
 
             var _updateStrength = function(score) {
-              console.log('update')
               if (score === 4) {
                 scope.strength = 'Great';
                 scope.strengthClass = 'success';
               } else if (score < 4 && score >= 2) {
-                scope.strength = 'So-so';
+                scope.strength = 'Ok';
                 scope.strengthClass = 'warning';
               } else {
                 scope.strength = 'Weak';
                 scope.strengthClass = 'danger';
               }
-              console.log('update end')
             };
-            // var clearFormErrors = function () {
-            //   angular.forEach(scope.ngModelCtrl.$error, function (value, name) {
-            //     scope.ngModelCtrl.$setValidity(name, null);
-            //   });
-            // };
           }
         };
       }
