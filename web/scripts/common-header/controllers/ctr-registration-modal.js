dfab900bf4b74c7de6d1ce01926f91d44fa564b5
santiagonoguez <santiago@nul-unu.com>
2019-11-15 11:07:50 -0600

    use scope variables [stage-9]

--- web/scripts/common-header/controllers/ctr-registration-modal.js
+++ web/scripts/common-header/controllers/ctr-registration-modal.js
@@ -35,12 +35,12 @@ angular.module('risevision.common.header')
         $scope.profile.mailSyncEnabled = false;
       }
 
-      $scope.profile.alreadyOptedIn = $scope.profile.mailSyncEnabled;
+      $scope.alreadyOptedIn = $scope.profile.mailSyncEnabled;
 
       if ($scope.newUser) {
-        getAccount($scope.profile.email).then(function (account) {
-          if (account && account.mailSyncEnabled) {
-            $scope.profile.alreadyOptedIn = true;
+        getAccount($scope.profile.email).then(function (subscriber) {
+          if (subscriber && subscriber.mailSyncEnabled) {
+            $scope.alreadyOptedIn = true;
           }
         });
       }
