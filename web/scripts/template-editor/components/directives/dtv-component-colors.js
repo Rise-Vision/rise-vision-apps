4ec5b24e4c8d661bff9dd71ab765030139e96a9c
Stuart Lees <stuartlees@gmail.com>
2020-02-04 11:59:36 -0500

    $watch the color picker $scope model values to update attribute data [stage-8]

--- web/scripts/template-editor/components/directives/dtv-component-colors.js
+++ web/scripts/template-editor/components/directives/dtv-component-colors.js
@@ -10,13 +10,9 @@ angular.module('risevision.template-editor.directives')
         link: function ($scope, element) {
           $scope.factory = templateEditorFactory;
 
-          $scope.save = function () {
-            $scope.setAttributeData($scope.componentId, 'base', $scope.baseColor);
-            $scope.setAttributeData($scope.componentId, 'accent', $scope.accentColor);
+          $scope.saveOverride = function () {
             $scope.setAttributeData($scope.componentId, 'override', $scope.override);
           };
-
-          $scope.$on('colorpicker-selected', $scope.save);
 
           $scope.registerDirective({
             type: 'rise-data-colors',
@@ -34,6 +30,18 @@ angular.module('risevision.template-editor.directives')
             $scope.baseColor = $scope.getAvailableAttributeData($scope.componentId, 'base');
             $scope.accentColor = $scope.getAvailableAttributeData($scope.componentId, 'accent');
             $scope.override = $scope.getAvailableAttributeData($scope.componentId, 'override');
+
+            $scope.$watch('baseColor', function(newVal, oldVal) {
+              if ( newVal && newVal !== oldVal ) {
+                $scope.setAttributeData($scope.componentId, 'base', $scope.baseColor);
+              }
+            });
+
+            $scope.$watch('accentColor', function(newVal, oldVal) {
+              if ( newVal && newVal !== oldVal ) {
+                $scope.setAttributeData($scope.componentId, 'accent', $scope.accentColor);
+              }
+            });
           };
 
         }
