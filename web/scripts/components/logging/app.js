16cac3a2876c8034f8ecca363f7043cf8f3dc1ff
Ezequiel Conte <ezequielc@gmail.com>
2020-02-07 14:43:46 -0300

    Fix lint [stage-19]

--- web/scripts/components/logging/app.js
+++ web/scripts/components/logging/app.js
@@ -10,7 +10,7 @@
   angular.module('risevision.common.config')
     .value('ENABLE_EXTERNAL_LOGGING', true)
     // Google Tag Manager Stage environment
-    .value('TAG_MANAGER_CONTAINER_ID', 'GTM-MZJ2NQX')
+    .value('TAG_MANAGER_CONTAINER_ID', 'GTM-MZJ2NQX');
 
   angular.module('risevision.common.components.logging', [
     'risevision.common.components.scrolling-list'
