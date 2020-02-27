94654c730ba996b7255d15e802fd13dd67e391ef
Oleg Khasimkhanov <olegkhasimkhanov@Olegs-MacBook-Pro.local>
2020-02-27 14:27:51 -0500

    Load a list of templates [stage-4]

--- web/scripts/template-editor/components/directives/dtv-component-playlist.js
+++ web/scripts/template-editor/components/directives/dtv-component-playlist.js
@@ -1,14 +1,16 @@
 'use strict';
 
 angular.module('risevision.template-editor.directives')
-  .directive('templateComponentPlaylist', ['templateEditorFactory', 'templateEditorUtils',
-    function (templateEditorFactory, templateEditorUtils) {
+  .directive('templateComponentPlaylist', ['templateEditorFactory', 'presentation', '$log',
+    function (templateEditorFactory, presentation, $log) {
       return {
         restrict: 'E',
         scope: true,
         templateUrl: 'partials/template-editor/components/component-playlist.html',
         link: function ($scope, element) {
           $scope.factory = templateEditorFactory;
+          $scope.searchKeyword = '';
+          $scope.templatesSearch = [];
 
           function _load() {
             //
@@ -28,6 +30,37 @@ angular.module('risevision.template-editor.directives')
               _load();
             }
           });
+
+          $scope.showAddTemplates = function () {
+            $scope.view = 'add-templates';
+            $scope.searchTemplates();
+          };
+
+          $scope.showSelectedTemplates = function () {
+            $scope.view = '';
+          };
+
+          $scope.searchTemplates = function () {
+
+            $scope.searching = true;
+            $scope.canAddTemplate = false;
+
+            var search = {
+              query: $scope.searchKeyword,
+              filter: 'presentationType:"HTML Template"'
+            };
+
+            presentation.list(search)
+            .then(function (result) {
+              $scope.templatesSearch = angular.copy(result.items);
+
+              $scope.searching = false;
+            })
+            .catch(function (err) {
+              $log.error(err);
+              $scope.searching = false;
+            });
+          };
 
         }
       };
