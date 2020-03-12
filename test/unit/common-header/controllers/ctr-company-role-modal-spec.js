16cb37dd7efa5aeb57ac934f8d8ef7bd2c6e5599
Alex Deaconu <alex.deaconu@gmail.com>
2020-03-12 10:12:51 -0400

    Show role selector for education users
    If the user has not yet selected a role
    Show the role selector after one day
    
    Refactor unit tests
    
    [stage-2]

---
+++ test/unit/common-header/controllers/ctr-company-role-modal-spec.js
@@ -0,0 +1,80 @@
+"use strict";
+
+/*jshint -W030 */
+
+describe("controller: Company Role Modal", function() {
+  beforeEach(module("risevision.common.header"));
+  beforeEach(module(function ($provide, $translateProvider) {
+    $provide.service("$modalInstance",function(){
+      return {
+        close: sinon.stub()
+      };
+    });
+    $provide.value("user", {
+      username: "user@example.io",
+      
+    });
+
+    $provide.factory("customLoader", function ($q) {
+      return function () {
+        var deferred = $q.defer();
+        deferred.resolve({});
+        return deferred.promise;
+      };
+    });
+
+    $translateProvider.useLoader("customLoader");
+  }));
+  var $scope, $modalInstance;
+  beforeEach(function(){
+    inject(function($injector,$rootScope, $controller){
+      $scope = $rootScope.$new();
+      $modalInstance = $injector.get("$modalInstance");
+
+      $controller("CompanyRoleModalCtrl", {
+        $scope : $scope,
+        $modalInstance: $modalInstance,
+      });
+      $scope.$digest();
+    });
+  });
+    
+  it("should exist", function() {
+    expect($scope).to.be.ok;
+    expect($scope.user).to.be.ok;
+
+    expect($scope).to.have.property("COMPANY_ROLE_FIELDS");
+
+    expect($scope.save).to.exist;
+    expect($scope.selectRole).to.exist;
+  });
+
+  it("should initialize", function() {
+    expect($scope.user.username).to.equal("user@example.io");
+    
+    expect($scope.COMPANY_ROLE_FIELDS).to.have.length(13);
+  });
+  
+  it("should close modal on save and send user objects", function() {
+    $scope.save();
+
+    $modalInstance.close.should.have.been.calledWith({
+      user: {
+        username: "user@example.io"
+      }
+    });
+  });
+  
+  it("selectRole: ", function() {
+    $scope.selectRole("Role1");
+    expect($scope.user.companyRole).to.equal("Role1");
+
+    $scope.selectRole("Role2");
+    expect($scope.user.companyRole).to.equal("Role2");
+
+    $scope.selectRole("Role2");
+    expect($scope.user.companyRole).to.equal("");
+  });
+
+});
+  
