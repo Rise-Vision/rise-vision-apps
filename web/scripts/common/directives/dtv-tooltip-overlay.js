72379d4863a85a54994484df774fbef96db7fe16
Alex Deaconu <alex.deaconu@gmail.com>
2020-06-18 14:32:18 -0400

    Fix linting [stage-2]

--- web/scripts/common/directives/dtv-tooltip-overlay.js
+++ web/scripts/common/directives/dtv-tooltip-overlay.js
@@ -8,14 +8,14 @@ angular.module('risevision.apps.directives')
         scope: {
           isShowing: '='
         },
-        terminal: true, //this setting is important, see explanation below
-        priority: 1000, //this setting is important, see explanation below
+        terminal: true,
+        priority: 1000,
         compile: function(element, attrs) {
           element.attr('tooltip-template', '"partials/launcher/share-tooltip.html"');
           element.attr('tooltip-placement', 'bottom');
           element.attr('tooltip-class', 'madero-style');
           element.attr('tooltip-trigger', 'show');
-          element.removeAttr("tooltip-overlay"); //remove the attribute to avoid indefinite loop
+          element.removeAttr('tooltip-overlay'); //remove the attribute to avoid infinite loop
 
           return {
             pre: function preLink($scope, iElement, iAttrs, controller) {  },
