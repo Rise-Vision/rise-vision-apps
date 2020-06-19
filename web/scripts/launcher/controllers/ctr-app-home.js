ccf75101364158dec81c259ab431d4ac25edd173
Alex Deaconu <alex.deaconu@gmail.com>
2020-06-19 10:07:16 -0400

    Store and retrieve value for overlay in local storage (#1754)
    [stage-2]

--- web/scripts/launcher/controllers/ctr-app-home.js
+++ web/scripts/launcher/controllers/ctr-app-home.js
@@ -1,10 +1,19 @@
 'use strict';
 
 angular.module('risevision.apps.launcher.controllers')
-  .controller('AppHomeCtrl', ['$scope', 'editorFactory',
-    function ($scope, editorFactory) {
+  .controller('AppHomeCtrl', ['$scope', 'localStorageService', 'editorFactory',
+    function ($scope, localStorageService, editorFactory) {
+      var tooltipDismissedKey = 'ShareTooltip.dismissed';
+
       $scope.editorFactory = editorFactory;
 
-      $scope.showTooltipOverlay = false;
+      $scope.showTooltipOverlay = localStorageService.get(tooltipDismissedKey) !== true;
+
+      if ($scope.showTooltipOverlay) {
+        var handler = $scope.$on('tooltipOverlay.dismissed', function() {
+          localStorageService.set(tooltipDismissedKey, true);
+          handler();
+        });        
+      }
     }
   ]); //ctr
