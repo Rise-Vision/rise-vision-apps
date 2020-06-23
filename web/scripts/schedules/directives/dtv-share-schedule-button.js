4bfc198ae8257cb0a3cf769fdfc830a84750bc7a
Ezequiel Conte <ezequielc@gmail.com>
2020-06-23 14:45:51 -0300

    Share Schedule button skeleton [stage-1]

---
+++ web/scripts/schedules/directives/dtv-share-schedule-button.js
@@ -0,0 +1,57 @@
+'use strict';
+
+angular.module('risevision.schedules.directives')
+.directive('shareScheduleButton', ['$timeout',
+    function ($timeout) {
+      return {
+        restrict: 'E',
+        templateUrl: 'partials/schedules/share-schedule-button.html',
+        scope: {
+          scheduleId: '='
+        },
+        link: function ($scope, element) {
+          var isTooltipOpen = false;
+          var tooltipButton = angular.element(element[0].querySelector('#tooltipButton'));
+
+          var isActionSheetOpen = false;
+          var actionSheetButton = angular.element(element[0].querySelector('#actionSheetButton'));
+
+          $scope.dismiss = function() {
+            $timeout(function() {
+              if (isTooltipOpen) {
+                isTooltipOpen = false;
+                tooltipButton.trigger('hide');
+              }
+
+              if(isActionSheetOpen) {
+                isActionSheetOpen = false;
+                actionSheetButton.trigger('toggle');
+              }
+            });
+          };
+
+          $scope.toggleTooltip = function() {
+            $timeout(function() {
+              if (isTooltipOpen) {
+                isTooltipOpen = false;
+                tooltipButton.trigger('hide');
+              } else {
+                isTooltipOpen = true;
+                tooltipButton.trigger('show');
+              }
+            });
+          };
+
+          $scope.toggleActionSheet = function() {
+            if (isActionSheetOpen) {
+              isActionSheetOpen = false;
+              actionSheetButton.trigger('toggle');
+            } else {
+              isActionSheetOpen = true;
+              actionSheetButton.trigger('toggle');
+            }
+          };
+        }
+      };
+    }
+  ]);
