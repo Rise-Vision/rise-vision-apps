e3d9f5790e9c007c0cb75bb1fb0b4753145600cd
duckbeartrap <vilsona97@gmail.com>
2020-06-29 20:25:21 +0400

    50 popular google fonts added to default fonts in dopdown (no preview) [stage-3]

--- web/scripts/template-editor/components/directives/dtv-component-text.js
+++ web/scripts/template-editor/components/directives/dtv-component-text.js
@@ -24,10 +24,12 @@ angular.module('risevision.template-editor.directives')
               'alignleft aligncenter alignright | ' +
               'removeformat',
             fontsize_formats: '24px 36px 48px 60px 72px 84px 96px 108px 120px 150px 200px 300px',
-            force_p_newlines : false,
-            forced_root_block : '',
-            setup: function(editor) {
-              editor.on('paste', function(e) {
+            force_p_newlines: false,
+            forced_root_block: '',
+            content_css: '@import url(https://fonts.googleapis.com/css2?family=Roboto&display=swap);',
+            font_formats: 'Andale Mono=andale mono,times; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Terminal=terminal,monaco; Times New Roman=times new roman,times; Trebuchet MS=trebuchet ms,geneva; Verdana=verdana,geneva; Webdings=webdings; Wingdings=wingdings,zapf dingbats; Roboto=roboto,sans-serif;Open Sans=open sans,sans-serif;Lato=lato,sans-serif;Noto Sans JP=noto sans jp,sans-serif;Montserrat=montserrat,sans-serif;Roboto Condensed=roboto condensed,sans-serif;Source Sans Pro=source sans pro,sans-serif;Oswald=oswald,sans-serif;Raleway=raleway,sans-serif;Roboto Mono=roboto mono,sans-serif;Poppins=poppins,sans-serif;Noto Sans=noto sans,sans-serif;Roboto Slab=roboto slab,sans-serif;Merriweather=merriweather,sans-serif;PT Sans=pt sans,sans-serif;Ubuntu=ubuntu,sans-serif;Playfair Display=playfair display,sans-serif;Muli=muli,sans-serif;Open Sans Condensed=open sans condensed,sans-serif;PT Serif=pt serif,sans-serif;Mukta=mukta,sans-serif;Nunito=nunito,sans-serif;Lora=lora,sans-serif;Work Sans=work sans,sans-serif;Fira Sans=fira sans,sans-serif;Rubik=rubik,sans-serif;Noto Sans KR=noto sans kr,sans-serif;Noto Serif=noto serif,sans-serif;Titillium Web=titillium web,sans-serif;Nanum Gothic=nanum gothic,sans-serif;Noto Sans TC=noto sans tc,sans-serif;Quicksand=quicksand,sans-serif;Nunito Sans=nunito sans,sans-serif;PT Sans Narrow=pt sans narrow,sans-serif;Heebo=heebo,sans-serif;Inconsolata=inconsolata,sans-serif;Barlow=barlow,sans-serif;Hind Siliguri=hind siliguri,sans-serif;Oxygen=oxygen,sans-serif;Arimo=arimo,sans-serif;Dosis=dosis,sans-serif;Libre Baskerville=libre baskerville,sans-serif;Crimson Text=crimson text,sans-serif;Karla=karla,sans-serif;Slabo 27px=slabo 27px,sans-serif;Josefin Sans=josefin sans,sans-serif;Bitter=bitter,sans-serif;Anton=anton,sans-serif;Libre Franklin=libre franklin,sans-serif;Cabin=cabin,sans-serif;',
+            setup: function (editor) {
+              editor.on('paste', function (e) {
                 //paste does not trigger change event
                 //editor.getContent() needs timeout in order to include the change
                 setTimeout(function () {
