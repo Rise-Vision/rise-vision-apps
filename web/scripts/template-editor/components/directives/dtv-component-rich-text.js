001576d605e4b7a51303ac1407b66198b774229e
Oleg Khasimkhanov <olegkhasimkhanov@Olegs-MacBook-Pro.local>
2020-07-03 13:51:02 -0400

    Remove link

--- web/scripts/template-editor/components/directives/dtv-component-rich-text.js
+++ web/scripts/template-editor/components/directives/dtv-component-rich-text.js
@@ -14,12 +14,11 @@ angular.module('risevision.template-editor.directives')
           /*jshint camelcase: false */
           $scope.tinymceOptions = {
             baseURL: '/vendor/tinymce/', //set path to load theme and skin files
-            plugins: 'colorpicker textcolor lists link',
+            plugins: 'colorpicker textcolor lists',
             menubar: false,
             toolbar1: 'fontselect fontsizeselect | ' +
               'bold italic underline | ' +
               'forecolor backcolor | ' +
-              'link | ' +
               'numlist bullist | ' +
               'alignleft aligncenter alignright | ' +
               'removeformat',
