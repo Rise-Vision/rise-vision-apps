0c076226ea4d9a62fc54c01117aaa548ff19975f
Oleg Khasimkhanov <olegkhasimkhanov@Olegs-MacBook-Pro.local>
2020-07-06 15:34:12 -0400

    Save google fonts [stage-4]

--- web/scripts/template-editor/components/directives/dtv-component-rich-text.js
+++ web/scripts/template-editor/components/directives/dtv-component-rich-text.js
@@ -2,7 +2,9 @@
 
 angular.module('risevision.template-editor.directives')
   .directive('templateComponentRichText', ['$timeout', '$window', 'templateEditorFactory',
-    function ($timeout, $window, templateEditorFactory) {
+    'COMMON_FONT_FAMILIES', 'GOOGLE_MOST_POPULAR_FONTS',
+    function ($timeout, $window, templateEditorFactory,
+      COMMON_FONT_FAMILIES, GOOGLE_MOST_POPULAR_FONTS) {
       return {
         restrict: 'E',
         scope: true,
@@ -25,8 +27,8 @@ angular.module('risevision.template-editor.directives')
             fontsize_formats: '24px 36px 48px 60px 72px 84px 96px 108px 120px 150px 200px 300px',
             force_p_newlines: false,
             forced_root_block: '',
-            content_style: '@import url(\'https://fonts.googleapis.com/css2?family=Roboto&family=Open+Sans&family=Lato&family=Noto+Sans+JP&family=Montserrat&family=Roboto+Condensed&family=Source+Sans+Pro&family=Oswald&family=Raleway&family=Roboto+Mono&family=Poppins&family=Noto+Sans&family=Roboto+Slab&family=Merriweather&family=PT+Sans&family=Ubuntu&family=Playfair+Display&family=Muli&family=Open+Sans+Condensed&family=PT+Serif&family=Mukta&family=Nunito&family=Lora&family=Work+Sans&family=Fira+Sans&family=Rubik&family=Noto+Sans+KR&family=Noto+Serif&family=Titillium+Web&family=Nanum+Gothic&family=Noto+Sans+TC&family=Quicksand&family=Nunito+Sans&family=PT+Sans+Narrow&family=Heebo&family=Inconsolata&family=Barlow&family=Hind+Siliguri&family=Oxygen&family=Arimo&family=Dosis&family=Libre+Baskerville&family=Crimson+Text&family=Karla&family=Slabo+27px&family=Josefin+Sans&family=Bitter&family=Anton&family=Libre+Franklin&family=Cabin\');',
-            font_formats: 'Andale Mono=andale mono,times;Arial=arial,helvetica,sans-serif;Arial Black=arial black,avant garde;Book Antiqua=book antiqua,palatino;Comic Sans MS=comic sans ms,sans-serif;Courier New=courier new,courier;Georgia=georgia,palatino;Helvetica=helvetica;Impact=impact,chicago;Symbol=symbol;Tahoma=tahoma,arial,helvetica,sans-serif;Terminal=terminal,monaco;Times New Roman=times new roman,times;Trebuchet MS=trebuchet ms,geneva;Verdana=verdana,geneva;Webdings=webdings;Wingdings=wingdings,zapf dingbats;Anton=anton,sans-serif;Arimo=arimo,sans-serif;Barlow=barlow,sans-serif;Bitter=bitter,sans-serif;Cabin=cabin,sans-serif;Crimson Text=crimson text,sans-serif;Dosis=dosis,sans-serif;Fira Sans=fira sans,sans-serif;Heebo=heebo,sans-serif;Hind Siliguri=hind siliguri,sans-serif;Inconsolata=inconsolata,sans-serif;Josefin Sans=josefin sans,sans-serif;Karla=karla,sans-serif;Lato=lato,sans-serif;Libre Baskerville=libre baskerville,sans-serif;Libre Franklin=libre franklin,sans-serif;Lora=lora,sans-serif;Merriweather=merriweather,sans-serif;Montserrat=montserrat,sans-serif;Mukta=mukta,sans-serif;Muli=muli,sans-serif;Nanum Gothic=nanum gothic,sans-serif;Noto Sans=noto sans,sans-serif;Noto Sans JP=noto sans jp,sans-serif;Noto Sans KR=noto sans kr,sans-serif;Noto Sans TC=noto sans tc,sans-serif;Noto Serif=noto serif,sans-serif;Nunito=nunito,sans-serif;Nunito Sans=nunito sans,sans-serif;Open Sans=open sans,sans-serif;Open Sans Condensed=open sans condensed,sans-serif;Oswald=oswald,sans-serif;Oxygen=oxygen,sans-serif;PT Sans=pt sans,sans-serif;PT Sans Narrow=pt sans narrow,sans-serif;PT Serif=pt serif,sans-serif;Playfair Display=playfair display,sans-serif;Poppins=poppins,sans-serif;Quicksand=quicksand,sans-serif;Raleway=raleway,sans-serif;Roboto=roboto,sans-serif;Roboto Condensed=roboto condensed,sans-serif;Roboto Mono=roboto mono,sans-serif;Roboto Slab=roboto slab,sans-serif;Rubik=rubik,sans-serif;Slabo 27px=slabo 27px,sans-serif;Source Sans Pro=source sans pro,sans-serif;Titillium Web=titillium web,sans-serif;Ubuntu=ubuntu,sans-serif;Work Sans=work sans,sans-serif;',
+            content_style: '@import url("' + getGoogleFontsUrl() + '");',
+            font_formats: getSortedFontFormats(),
             setup: function (editor) {
               editor.on('paste', function (e) {
                 //paste does not trigger change event
@@ -50,7 +52,46 @@ angular.module('risevision.template-editor.directives')
 
           $scope.save = function () {
             $scope.setAttributeData($scope.componentId, 'richtext', $scope.data.richText);
+            $scope.setAttributeData($scope.componentId, 'googlefonts', getGoogleFontsUsed(getAllFontsUsed($scope
+              .data.richText)));
           };
+
+          function getAllFontsUsed(richText) {
+            var wrapper = document.createElement('div'),
+              families = '',
+              $wrapper;
+
+            wrapper.innerHTML = richText;
+            $wrapper = angular.element(wrapper);
+
+            // go through fonts applied in html to prepare a list to find out which ones are google fonts
+            angular.forEach($wrapper.find('span'), function (span) {
+              var family = angular.element(span).css('font-family');
+              // remove single quotes (if applied) and fallback fonts
+              if (family) {
+                family = family.replace(/[']/g, '').split(',')[0];
+
+                if (families.indexOf(family) === -1) {
+                  // add font family to the list
+                  families += family + ',';
+                }
+              }
+            });
+
+            return families;
+          }
+
+          function getGoogleFontsUsed(fontsUsed) {
+            var googleFontsUsed = [];
+
+            angular.forEach(GOOGLE_MOST_POPULAR_FONTS, function (family) {
+              if (fontsUsed.indexOf(family) !== -1) {
+                googleFontsUsed.push(family);
+              }
+            });
+
+            return googleFontsUsed;
+          }
 
           $scope.registerDirective({
             type: 'rise-rich-text',
@@ -63,7 +104,99 @@ angular.module('risevision.template-editor.directives')
             }
           });
 
+          function getSortedFontFormats() {
+            var allFonts = COMMON_FONT_FAMILIES.concat(getGoogleFontsFamilies());
+            return allFonts.sort().join('');
+          }
+
+          function getGoogleFontsUrl() {
+            return 'https://fonts.googleapis.com/css2?family=' +
+              encodeURI(GOOGLE_MOST_POPULAR_FONTS.join('&family='));
+          }
+
+          function getGoogleFontsFamilies() {
+            var result = [];
+            var fallback = ',sans-serif;';
+
+            angular.forEach(GOOGLE_MOST_POPULAR_FONTS, function (family) {
+              result.push(family + '=' + family + fallback);
+            });
+
+            return result;
+          }
+
         }
       };
     }
-  ]);
+  ])
+  .constant('COMMON_FONT_FAMILIES',
+    [
+      'Andale Mono=andale mono,monospace;',
+      'Arial=arial,helvetica,sans-serif;',
+      'Arial Black=arial black,sans-serif;',
+      'Book Antiqua=book antiqua,palatino,serif;',
+      'Comic Sans MS=comic sans ms,sans-serif;',
+      'Courier New=courier new,courier,monospace;',
+      'Georgia=georgia,palatino,serif;',
+      'Helvetica=helvetica,arial,sans-serif;',
+      'Impact=impact,sans-serif;',
+      'Symbol=symbol;',
+      'Tahoma=tahoma,arial,helvetica,sans-serif;',
+      'Terminal=terminal,monaco,monospace;',
+      'Times New Roman=times new roman,times,serif;',
+      'Trebuchet MS=trebuchet ms,geneva,sans-serif;',
+      'Verdana=verdana,geneva,sans-serif;'
+    ])
+  .constant('GOOGLE_MOST_POPULAR_FONTS',
+    [
+      'Roboto',
+      'Open Sans',
+      'Lato',
+      'Noto Sans JP',
+      'Montserrat',
+      'Roboto Condensed',
+      'Source Sans Pro',
+      'Oswald',
+      'Raleway',
+      'Roboto Mono',
+      'Poppins',
+      'Noto Sans',
+      'Roboto Slab',
+      'Merriweather',
+      'PT Sans',
+      'Ubuntu',
+      'Playfair Display',
+      'Muli',
+      'Open Sans Condensed',
+      'PT Serif',
+      'Mukta',
+      'Nunito',
+      'Lora',
+      'Work Sans',
+      'Fira Sans',
+      'Rubik',
+      'Noto Sans KR',
+      'Noto Serif',
+      'Titillium Web',
+      'Nanum Gothic',
+      'Noto Sans TC',
+      'Quicksand',
+      'Nunito Sans',
+      'PT Sans Narrow',
+      'Heebo',
+      'Inconsolata',
+      'Barlow',
+      'Hind Siliguri',
+      'Oxygen',
+      'Arimo',
+      'Dosis',
+      'Libre Baskerville',
+      'Crimson Text',
+      'Karla',
+      'Slabo 27px',
+      'Josefin Sans',
+      'Bitter',
+      'Anton',
+      'Libre Franklin',
+      'Cabin'
+    ]);
