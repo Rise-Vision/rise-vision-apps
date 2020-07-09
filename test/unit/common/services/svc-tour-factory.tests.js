9a6ecfd702f935ef80af6d9d1c53d7ec9fd2c7ee
Alex Deaconu <alex.deaconu@gmail.com>
2020-07-09 13:48:08 -0400

    Show tours for 5 separate sessions
    [stage-19]

--- test/unit/common/services/svc-tour-factory.tests.js
+++ test/unit/common/services/svc-tour-factory.tests.js
@@ -1,7 +1,7 @@
 'use strict';
 describe('service: tourFactory:', function() {
   beforeEach(module('risevision.apps.services'));
-  var tourFactory, localStorageService;
+  var tourFactory, localStorageService, $sessionStorage;
   beforeEach(function(){
     module(function ($provide) {
       $provide.service('localStorageService', function() {
@@ -10,11 +10,13 @@ describe('service: tourFactory:', function() {
           set: sinon.stub()
         };
       });
+      $provide.value('$sessionStorage', {});
     })
 
     inject(function($injector) {
       tourFactory = $injector.get('tourFactory');
       localStorageService = $injector.get('localStorageService');
+      $sessionStorage = $injector.get('$sessionStorage');
     });
   });
 
@@ -25,26 +27,56 @@ describe('service: tourFactory:', function() {
   });
 
   describe('isShowing:', function() {
-    it("should check local storage",function(){
-      localStorageService.get.returns('anything');
+    beforeEach(function() {
+      localStorageService.get.reset();
+      localStorageService.set.reset();
+    });
+
+    it("should check session storage if the tour was seen",function(){
+      $sessionStorage.tooltipKeySeen = true;
+
+      expect(tourFactory.isShowing('tooltipKey')).to.be.false;
+
+      localStorageService.get.should.not.have.been.called;
+      localStorageService.set.should.not.have.been.called;
+    });
+
+    it("should check how many times the tour was seen",function(){
+      $sessionStorage.tooltipKeySeen = false;
+      localStorageService.get.returns(6);
+
+      expect(tourFactory.isShowing('tooltipKey')).to.be.false;
+
+      localStorageService.get.should.have.been.calledWith('tooltipKeySeen');
+      localStorageService.set.should.not.have.been.called;
+    });
+
+    it("should show and initialize the number of times the tour was seen",function(){
+      $sessionStorage.tooltipKeySeen = false;
+      localStorageService.get.returns(null);
 
       expect(tourFactory.isShowing('tooltipKey')).to.be.true;
 
-      localStorageService.get.should.have.been.calledWith('tooltipKey.dismissed');
+      localStorageService.get.should.have.been.calledWith('tooltipKeySeen');
+      localStorageService.set.should.have.been.calledWith('tooltipKeySeen', 1);
     });
 
-    it("should not show if already dismissed",function(){
-      localStorageService.get.returns(true);
+    it("should show and increment the number of times the tour was seen",function(){
+      $sessionStorage.tooltipKeySeen = false;
+      localStorageService.get.returns(2);
 
-      expect(tourFactory.isShowing('tooltipKey')).to.be.false;
+      expect(tourFactory.isShowing('tooltipKey')).to.be.true;
+
+      localStorageService.get.should.have.been.calledWith('tooltipKeySeen');
+      localStorageService.set.should.have.been.calledWith('tooltipKeySeen', 3);
     });
   });
 
   describe('dismissed:', function() {
-    it('should update local storage', function() {     
+    it('should update session storage', function() {     
       tourFactory.dismissed('tooltipKey');
 
-      localStorageService.set.should.have.been.calledWith('tooltipKey.dismissed', true);
+      expect($sessionStorage.tooltipKeySeen).to.be.true;
     });
   });
 
