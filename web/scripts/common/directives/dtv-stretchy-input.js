c0aed78b50401b04b74645a2aa65b96b0b552577
Alex Deaconu <alex.deaconu@gmail.com>
2020-07-15 14:44:13 -0400

    Create generic stretchy input for editing
    Update Name input on schedule page
    Various other styling updates and fixes
    
    [stage-19]

---
+++ web/scripts/common/directives/dtv-stretchy-input.js
@@ -0,0 +1,77 @@
+'use strict';
+
+angular.module('risevision.apps.directives')
+  .directive('stretchyInput', ['$window', '$timeout',
+    function ($window, $timeout) {
+      return {
+        restrict: 'E',
+        require: 'ngModel',
+        scope: {
+          ngModel: '='
+        },
+        templateUrl: 'partials/common/stretchy-input.html',
+        link: function ($scope, element) {
+          $scope.isEditingInput = false;
+          $scope.defaultNameValue = $scope.ngModel;
+          $scope.defaultNameWidth = '';
+
+          var _initStretchy = function () {
+            var stretchyInputElement = element.find('input.input-stretchy');
+
+            $window.Stretchy.resize(stretchyInputElement[0]);
+
+            if (!$scope.defaultNameWidth) {
+              // first time editing, store the width of the field when default name is displayed
+              $scope.defaultNameWidth = $window.getComputedStyle(stretchyInputElement[0]).getPropertyValue('width');
+            }
+          };
+
+          $timeout(_initStretchy);
+
+          $scope.$watch('isEditingInput', function (editing) {
+            var stretchyInputElement = element.find('input.input-stretchy');
+
+            if (editing) {
+              setFocus(stretchyInputElement[0]);
+            } else {
+              var nameVal = stretchyInputElement[0].value.replace(/\s/g, '');
+
+              if (!nameVal) {
+                // user deleted entire name, set the name and width values to the defaults
+                $scope.ngModel = $scope.defaultNameValue;
+                stretchyInputElement[0].style.width = $scope.defaultNameWidth;
+              }
+            }
+          });
+
+          $scope.onInputBlur = function () {
+            $scope.isEditingInput = false;
+          };
+
+          $scope.inputKeyUp = function (keyEvent) {
+            // handle enter key
+            if (keyEvent.which === 13 && $scope.isEditingInput) {
+              $scope.isEditingInput = false;
+            }
+          };
+
+          function setFocus(elem) {
+            if (elem !== null) {
+              if (elem.createTextRange) {
+                var range = elem.createTextRange();
+                range.move('character', 0);
+                range.select();
+              } else {
+                if (elem.setSelectionRange) {
+                  elem.focus();
+                  elem.setSelectionRange(0, elem.value.length);
+                } else {
+                  elem.focus();
+                }
+              }
+            }
+          }
+        }
+      };
+    }
+  ]);
