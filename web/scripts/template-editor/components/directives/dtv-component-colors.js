1731028fa3caeb7f21acd2ce5e5870d93b984fdc
Stuart Lees <stuartlees@gmail.com>
2020-07-22 10:26:24 -0400

    Fix type value [stage-8]

--- web/scripts/template-editor/components/directives/dtv-component-colors.js
+++ web/scripts/template-editor/components/directives/dtv-component-colors.js
@@ -17,7 +17,7 @@ angular.module('risevision.template-editor.directives')
           };
 
           $scope.registerDirective({
-            type: 'rise-override-branding-colors',
+            type: 'rise-override-brand-colors',
             iconType: 'streamline',
             icon: 'palette',
             element: element,
