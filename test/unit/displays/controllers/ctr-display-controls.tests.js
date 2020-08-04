1841b51721a5023b15f38c410dfc4932a7ea2b30
Alex Deaconu <alex.deaconu@gmail.com>
2020-08-04 11:05:04 -0400

    Add button and dropdown for display actions
    Same behaviour as before but show actions in a dropdown
    
    Styling for mobile views and spacing fixes
    
    Use service to open modal
    
    [stage-2]

--- test/unit/displays/controllers/ctr-display-controls.tests.js
+++ test/unit/displays/controllers/ctr-display-controls.tests.js
@@ -44,29 +44,14 @@ describe('controller: display controls', function() {
     $provide.service('processErrorCode', function() {
       return processErrorCode = sinon.spy(function() { return 'error'; });
     });
-    $provide.service('$modal',function(){
-      return {
-        open : sinon.spy(function(obj) {
-          expect(obj).to.be.ok;
-          var deferred = Q.defer();
-          if(confirmResponse){
-            deferred.resolve();
-          }else{
-            deferred.reject();
-          }
-          
-          return {
-            result: deferred.promise
-          };
-        })
-      }
+    $provide.service('confirmModal',function(){
+      return sinon.stub().returns(Q.reject());
     });
   }));
-  var $scope, $modal, userState, $location, updateDisplay, confirmResponse, functionCalled,
+  var $scope, confirmModal, userState, $location, updateDisplay, functionCalled,
   trackerCalled, processErrorCode, displayFactory;
   beforeEach(function(){
     updateDisplay = true;
-    confirmResponse = false;
     functionCalled = undefined;
     trackerCalled = undefined;
     
@@ -87,13 +72,13 @@ describe('controller: display controls', function() {
       $scope = $rootScope.$new();
       $location = $injector.get('$location');
       displayFactory = $injector.get('displayFactory');
-      $modal = $injector.get('$modal');
+      confirmModal = $injector.get('confirmModal');
 
       $controller('displayControls', {
         $scope : $scope,
         userState : $injector.get('userState'),
         display:$injector.get('display'),
-        $modal:$modal,
+        confirmModal:confirmModal,
         $log : $injector.get('$log')});
       $scope.$digest();
     });
@@ -111,40 +96,38 @@ describe('controller: display controls', function() {
 
       $scope.confirm('1234', 'restart');
 
-      $modal.open.should.not.have.been.called;
+      confirmModal.should.not.have.been.called;
       expect(functionCalled).to.not.be.ok;
     });
 
     it('should return early the user does not confirm',function(){
       $scope.confirm('1234', 'restart');
       
-      $modal.open.should.have.been.called;
+      confirmModal.should.have.been.called;
       expect(functionCalled).to.not.be.ok;
     });
     
     it('should restart the display',function(done){
-      confirmResponse = true;
+      confirmModal.returns(Q.resolve());
       updateDisplay = true;
       
       $scope.confirm('1234', 'Display 1', 'restart');
       setTimeout(function(){
         expect(functionCalled).to.equal('restart');
         expect(trackerCalled).to.equal('Display Restarted');
-        expect($scope.controlsInfo).to.be.ok;
         expect($scope.controlsError).to.not.be.ok;
         done();
       },10);
     });
     
     it('should show an error if fails to restart the display',function(done){
-      confirmResponse = true;
+      confirmModal.returns(Q.resolve());
       updateDisplay = false;
       
       $scope.confirm('1234', 'Display 1', 'restart');
       setTimeout(function(){
         expect(functionCalled).to.equal('restart');
         expect(trackerCalled).to.not.be.ok;
-        expect($scope.controlsInfo).to.not.be.ok;
         processErrorCode.should.have.been.calledWith('Display', 'restart', sinon.match.any);
         expect($scope.controlsError).to.be.ok;
         done();
@@ -158,40 +141,38 @@ describe('controller: display controls', function() {
 
       $scope.confirm('1234', 'reboot');
 
-      $modal.open.should.not.have.been.called;
+      confirmModal.should.not.have.been.called;
       expect(functionCalled).to.not.be.ok;
     });
 
     it('should return early the user does not confirm', function () {
       $scope.confirm('1234', 'reboot');
 
-      $modal.open.should.have.been.called;
+      confirmModal.should.have.been.called;
       expect(functionCalled).to.not.be.ok;
     });
 
     it('should reboot the display', function (done) {
-      confirmResponse = true;
+      confirmModal.returns(Q.resolve());
       updateDisplay = true;
       
       $scope.confirm('1234', 'Display 1', 'reboot');
       setTimeout(function(){
         expect(functionCalled).to.equal('reboot');
         expect(trackerCalled).to.equal('Display Rebooted');
-        expect($scope.controlsInfo).to.be.ok;
         expect($scope.controlsError).to.not.be.ok;
         done();
       }, 10);
     });
 
     it('should show an error if fails to reboot the display', function (done) {
-      confirmResponse = true;
+      confirmModal.returns(Q.resolve());
       updateDisplay = false;
       
       $scope.confirm('1234', 'Display 1', 'reboot');
       setTimeout(function(){
         expect(functionCalled).to.equal('reboot');
         expect(trackerCalled).to.not.be.ok;
-        expect($scope.controlsInfo).to.not.be.ok;
         processErrorCode.should.have.been.calledWith('Display', 'reboot', sinon.match.any);
         expect($scope.controlsError).to.be.ok;
         done();
@@ -199,26 +180,24 @@ describe('controller: display controls', function() {
     });
 
     it('should do nothing when calling restart with undefined display id', function (done) {
-      confirmResponse = true;
+      confirmModal.returns(Q.resolve());
       updateDisplay = false;
 
       $scope.confirm(undefined, 'restart');
       setTimeout(function () {
         expect(functionCalled).to.not.be.ok;
-        expect($scope.controlsInfo).to.not.be.ok;
         expect($scope.controlsError).to.not.be.ok;
         done();
       }, 10);
     });
 
     it('should do nothing when calling reboot with undefined display id', function (done) {
-      confirmResponse = true;
+      confirmModal.returns(Q.resolve());
       updateDisplay = false;
 
       $scope.confirm(undefined, 'reboot');
       setTimeout(function () {
         expect(functionCalled).to.not.be.ok;
-        expect($scope.controlsInfo).to.not.be.ok;
         expect($scope.controlsError).to.not.be.ok;
         done();
       }, 10);
