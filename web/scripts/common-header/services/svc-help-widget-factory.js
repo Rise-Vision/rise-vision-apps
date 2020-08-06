04de23412d44d6f12f751b2dee68e80764466656
Ezequiel Conte <ezequielc@gmail.com>
2020-08-06 16:24:22 -0300

    Elevio unit tests and refactorings (#1942)
    * Address PR comments:
    - rename factory
    - remove button controller
    - remove $q.when()
    
    * Fix unit tests
    * gulp pretty
    * Fix e2e failures
    [stage-1]

--- web/scripts/common-header/services/svc-help-widget-factory.js
+++ web/scripts/common-header/services/svc-help-widget-factory.js
@@ -7,44 +7,42 @@
     .value('HELP_WIDGET_SCRIPT',
       '!function(e,l,v,i,o,n){e[i]||(e[i]={}),e[i].account_id=n;var g,h;g=l.createElement(v),g.type="text/javascript",g.async=1,g.src=o+n,h=l.getElementsByTagName(v)[0],h.parentNode.insertBefore(g,h);e[i].q=[];e[i].on=function(z,y){e[i].q.push([z,y])}}(window,document,"script","_elev","https://cdn.elev.io/sdk/bootloader/v4/elevio-bootloader.js?cid=","5f2331387a97f");'
     )
-    .factory('helpWidget', ['$q', '$window', 'userState', 'HELP_WIDGET_SCRIPT',
-      function ($q, $window, userState, HELP_WIDGET_SCRIPT) {
+    .factory('helpWidgetFactory', ['$window', 'HELP_WIDGET_SCRIPT',
+      function ($window, HELP_WIDGET_SCRIPT) {
         var loaded = false;
 
-        function ensureScript() {
+        function initializeWidget() {
           if (!loaded) {
             var scriptElem = $window.document.createElement('script');
             scriptElem.innerText = HELP_WIDGET_SCRIPT;
 
             $window.document.body.appendChild(scriptElem);
-            $window._elev.on('load', function(_elev) {
+            $window._elev.on('load', function (_elev) {
               _elev.setSettings({
                 hideLauncher: true
               });
             });
             loaded = true;
           }
-          return $q.when();
         }
-
-        function initializeWidget() {
-          return ensureScript();
-        }
-
 
         function showWidgetButton() {
           if ($window._elev) {
-            $window._elev.setSettings({hideLauncher: false});
+            $window._elev.setSettings({
+              hideLauncher: false
+            });
           }
         }
 
         function hideWidgetButton() {
           if ($window._elev) {
-            $window._elev.setSettings({hideLauncher: true});
+            $window._elev.setSettings({
+              hideLauncher: true
+            });
           }
         }
 
-        function activateWidget() {
+        function showHelpWidget() {
           if ($window._elev) {
             $window._elev.openHome();
           }
@@ -54,16 +52,16 @@
           initializeWidget: initializeWidget,
           showWidgetButton: showWidgetButton,
           hideWidgetButton: hideWidgetButton,
-          activateWidget: activateWidget
+          showHelpWidget: showHelpWidget
         };
 
       }
     ])
 
-    .run(['$rootScope', '$window', 'userState', 'userAuthFactory', 'helpWidget', 'HELP_WIDGET_SCRIPT',
-      function ($rootScope, $window, userState, userAuthFactory, helpWidget, HELP_WIDGET_SCRIPT) {
+    .run(['$rootScope', '$window', 'userState', 'userAuthFactory', 'helpWidgetFactory', 'HELP_WIDGET_SCRIPT',
+      function ($rootScope, $window, userState, userAuthFactory, helpWidgetFactory, HELP_WIDGET_SCRIPT) {
         if (HELP_WIDGET_SCRIPT) {
-          helpWidget.initializeWidget();
+          helpWidgetFactory.initializeWidget();
 
           userAuthFactory.authenticate()
             .then(function () {
@@ -85,11 +83,11 @@
         }
 
         function _hideHelpWidgetButton() {
-          helpWidget.hideWidgetButton();
+          helpWidgetFactory.hideWidgetButton();
         }
 
         function _showHelpWidgetButton() {
-          helpWidget.showWidgetButton();
+          helpWidgetFactory.showWidgetButton();
         }
       }
     ]);
