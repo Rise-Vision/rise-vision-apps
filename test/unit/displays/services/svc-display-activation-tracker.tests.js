18db09d0ca1e98238d8bb638a6d3adb8342cb769
Alex Deaconu <alex.deaconu@gmail.com>
2020-09-01 15:01:55 -0400

    Detect and track display activation
    Check both when loading displays and display get
    Check user settings to see if activation was tracked
    Do not track if in a subcompany
    Track as event and identify property
    Track date of the earliest active display date
    
    [stage-2]

---
+++ test/unit/displays/services/svc-display-activation-tracker.tests.js
@@ -0,0 +1,156 @@
+'use strict';
+describe('service: display activation tracker', function() {
+  beforeEach(module('risevision.displays.services'));
+
+  beforeEach(module(function ($provide) {
+    $provide.service('userState',function(){
+      return {
+        getUsername: sinon.stub().returns('username'), 
+        isSubcompanySelected: sinon.stub().returns(false),
+        getCopyOfProfile: sinon.stub().returns({}),
+        updateUserProfile: sinon.stub(),
+        _restoreState: function(){}
+      }
+    });
+    $provide.service('analyticsFactory',function(){
+      return {
+        identify: sinon.stub(), 
+        load: function(){}
+      }
+    });
+    $provide.service('updateUser', function() {
+      return sinon.stub().returns(Q.resolve({
+        item: 'updatedUser'
+      }));
+    });
+    $provide.service('displayTracker', function() {
+      return sinon.stub();
+    });
+
+  }));
+  
+  var displayActivationTracker, userState, analyticsFactory, updateUser, displayTracker;
+  beforeEach(function(){
+    inject(function($injector){
+      userState = $injector.get('userState');
+      analyticsFactory = $injector.get('analyticsFactory');
+      updateUser = $injector.get('updateUser');
+      displayTracker = $injector.get('displayTracker');
+      displayActivationTracker = $injector.get('displayActivationTracker');
+    });
+  });
+
+  it('should exist',function(){
+    expect(displayActivationTracker).to.be.ok;
+    expect(displayActivationTracker).to.be.a('function');
+  });
+  
+  it('should return early if subcompany is selected',function(){
+    userState.isSubcompanySelected.returns(true);
+
+    displayActivationTracker([]);
+
+    userState.isSubcompanySelected.should.have.been.called;
+    userState.getCopyOfProfile.should.not.have.been.called;
+
+    analyticsFactory.identify.should.not.have.been.called;
+    displayTracker.should.not.have.been.called;
+  });
+
+  it('should return early if setting exists',function(){
+    userState.getCopyOfProfile.returns({
+      settings: {
+        firstDisplayActivationDate: '2020-08-21T15:19:02.716Z'
+      }
+    });
+
+    displayActivationTracker([{
+      lastConnectionTime: '2020-08-21T15:19:02.716Z'
+    }]);
+
+    userState.isSubcompanySelected.should.have.been.called;
+    userState.getCopyOfProfile.should.have.been.called;
+
+    analyticsFactory.identify.should.not.have.been.called;
+    displayTracker.should.not.have.been.called;
+  });
+
+  describe('displays list:', function() {
+    it('should not track if lastConnectionTime is not available',function(){
+      displayActivationTracker([{
+        id: 'displayId1',
+        name: 'displayName1'
+      },{
+        id: 'displayId2',
+        name: 'displayName2'
+      }]);
+
+      userState.isSubcompanySelected.should.have.been.called;
+      userState.getCopyOfProfile.should.have.been.called;
+
+      analyticsFactory.identify.should.not.have.been.called;
+      displayTracker.should.not.have.been.called;
+    });
+
+    it('should track earliest lastConnectionTime',function(){
+      displayActivationTracker([{
+        id: 'displayId1',
+        name: 'displayName1',
+        lastConnectionTime: '2019-08-21T15:19:02.716Z'
+      },{
+        id: 'displayId2',
+        name: 'displayName2',
+        lastConnectionTime: '2020-08-21T15:19:02.716Z'
+      },{
+        id: 'displayId3',
+        name: 'displayName3',
+        lastConnectionTime: '2017-08-21T15:19:02.716Z'
+      }]);
+
+      analyticsFactory.identify.should.have.been.calledWith('username', {
+        firstDisplayActivationDate: '2017-08-21T15:19:02.716Z'
+      });
+      displayTracker.should.have.been.calledWith('first display activated', 'displayId3', 'displayName3', {
+        firstDisplayActivationDate: '2017-08-21T15:19:02.716Z'
+      });
+    });
+
+  });
+
+  it('should track event, send identify',function(){
+    displayActivationTracker([{
+      id: 'displayId',
+      name: 'displayName',
+      lastConnectionTime: '2020-08-21T15:19:02.716Z'
+    }]);
+
+    analyticsFactory.identify.should.have.been.calledWith('username', {
+      firstDisplayActivationDate: '2020-08-21T15:19:02.716Z'
+    });
+    displayTracker.should.have.been.calledWith('first display activated', 'displayId', 'displayName', {
+      firstDisplayActivationDate: '2020-08-21T15:19:02.716Z'
+    });
+
+  });
+
+  it('should update user settings',function(done){
+    displayActivationTracker([{
+      id: 'displayId',
+      name: 'displayName',
+      lastConnectionTime: '2020-08-21T15:19:02.716Z'
+    }]);
+
+    setTimeout(function() {
+      updateUser.should.have.been.calledWith('username', {
+        settings: {
+          firstDisplayActivationDate: '2020-08-21T15:19:02.716Z'
+        }
+      });
+      userState.updateUserProfile.should.have.been.calledWith('updatedUser');
+
+      done();
+    }, 10);
+
+  });
+
+});
--- web/scripts/components/logging/svc-display-tracker.js
+++ web/scripts/components/logging/svc-display-tracker.js
@@ -2,21 +2,26 @@
 
 angular.module('risevision.common.components.logging')
   .value('DISPLAY_EVENTS_TO_BQ', [
-    'Display Created',
-    'Player Download'
+    'Display Created'
   ])
   .factory('displayTracker', ['userState', 'analyticsFactory',
     'bigQueryLogging', 'DISPLAY_EVENTS_TO_BQ',
     function (userState, analyticsFactory, bigQueryLogging,
       DISPLAY_EVENTS_TO_BQ) {
-      return function (eventName, displayId, displayName, downloadType) {
+      return function (eventName, displayId, displayName, eventProperties) {
         if (eventName) {
-          analyticsFactory.track(eventName, {
+          eventProperties = eventProperties || {};
+          angular.extend(eventProperties, {
             displayId: displayId,
             displayName: displayName,
+            userId: userState.getUsername(),
+            email: userState.getUserEmail(),
             companyId: userState.getSelectedCompanyId(),
-            downloadType: downloadType
+            companyName: userState.getSelectedCompanyName()
           });
+
+          analyticsFactory.track(eventName, eventProperties);
+
           if (DISPLAY_EVENTS_TO_BQ.indexOf(eventName) !== -1) {
             bigQueryLogging.logEvent(eventName, displayId);
           }
