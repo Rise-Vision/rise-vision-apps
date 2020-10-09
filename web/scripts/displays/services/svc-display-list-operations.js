34725ffa610a971cd9280127a1619fab1824ec6a
Ezequiel Conte <ezequielc@gmail.com>
2020-10-09 14:24:20 -0300

    License Required modals for bulk actions Refactoring Restart/Reboot modals [stage-1]

--- web/scripts/displays/services/svc-display-list-operations.js
+++ web/scripts/displays/services/svc-display-list-operations.js
@@ -1,32 +1,61 @@
 'use strict';
 
 angular.module('risevision.displays.services')
-  .service('DisplayListOperations', ['displayFactory', 'playerActionsFactory', 'confirmModal', '$q',
-    function (displayFactory, playerActionsFactory, confirmModal, $q) {
+  .service('DisplayListOperations', ['displayFactory', 'playerActionsFactory', 'confirmModal', '$q', 'plansFactory', 'playerLicenseFactory',
+    function (displayFactory, playerActionsFactory, confirmModal, $q, plansFactory, playerLicenseFactory) {
       return function () {
         var _checkLicenses = function(selectedItems) {
-          //TODO
+          var notAuthorized = [];
+          angular.forEach(selectedItems, function(display) {
+            if (!display.playerProAuthorized) {
+              notAuthorized.push(display.id);
+            }
+          });
+          if (notAuthorized.length > 0) {
+            var availableLicenses = playerLicenseFactory.getProAvailableLicenseCount();
+            var baseMessage = notAuthorized.length > 1 ? 
+              notAuthorized.length + ' of your selected displays are not licensed and to perform this action they need to be.' :
+              '1 of your selected displays is not licensed and to perform this action it needs to be.';
+            baseMessage += ' You have '+ availableLicenses +' available license' + (availableLicenses > 1 ? 's' : '');
+
+            if (availableLicenses >= notAuthorized.length) {
+              return confirmModal('Almost there!', 
+                baseMessage + '. Do you want to assign '+notAuthorized.length+ (notAuthorized.length > 1 ? ' to these displays?' : ' to this display?'),
+                'Yes', 'No', 'madero-style centered-modal',
+                'partials/components/confirm-modal/madero-confirm-modal.html','sm').then(function() {
+                  // TODO: bulk assign
+                });
+            } else {
+              return confirmModal('Almost there!', 
+                baseMessage + ' and you need to subscribe for '+ (notAuthorized.length - availableLicenses) +' more to license ' + (notAuthorized.length > 1 ? 'these displays.' : 'this display.') ,
+                'Subscribe', 'Cancel', 'madero-style centered-modal',
+                'partials/components/confirm-modal/madero-confirm-modal.html','sm').then(function() {
+                   plansFactory.showPurchaseOptions();
+                   return $q.reject();
+                });
+            }
+          }
           return $q.resolve();
         };
 
-        var _confirmRestart = function(selectedItems) {
+        var _confirmPlayerAction = function(selectedItems, isRestart) {         
           var suffix = selectedItems.length > 1? 's' : '';
+          var title = isRestart ? 'Restart Rise Player'+ suffix +'?' :  'Reboot media player'+ suffix +'?';
+          var message = isRestart ? 'Rise Player'+ suffix +' will restart' : 'The media player'+ suffix +' will reboot';
+          message += ' and the content showing on your display'+ suffix +' will be interrupted. Do you wish to proceed?';
+
           return _checkLicenses(selectedItems).then(function() {
-            return confirmModal('Restart Rise Player'+ suffix +'?',
-              'Rise Player'+ suffix +' will restart and the content showing on your display'+ suffix +' will be interrupted. Do you wish to proceed?',
-              'Yes', 'No', 'madero-style centered-modal',
+            return confirmModal(title, message, 'Yes', 'No', 'madero-style centered-modal',
               'partials/components/confirm-modal/madero-confirm-modal.html','sm');
           });
         };
 
+        var _confirmRestart = function(selectedItems) {
+          return _confirmPlayerAction(selectedItems, true);
+        };
+
         var _confirmReboot = function(selectedItems) {
-          var suffix = selectedItems.length > 1? 's' : '';
-          return _checkLicenses(selectedItems).then(function() {
-            return confirmModal('Reboot media player'+ suffix +'?',
-              'The media player'+ suffix +' will reboot and the content showing on your display'+ suffix +' will be interrupted. Do you wish to proceed?',
-              'Yes', 'No', 'madero-style centered-modal',
-              'partials/components/confirm-modal/madero-confirm-modal.html','sm');
-          });
+          return _confirmPlayerAction(selectedItems, false);
         };
 
         var listOperations = {
