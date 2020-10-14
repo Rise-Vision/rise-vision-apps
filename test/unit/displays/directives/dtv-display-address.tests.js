12c47542538301b1807605c512f966450f56f7c7
Ezequiel Conte <ezequielc@gmail.com>
2020-10-14 15:19:13 -0300

    Unit tests for Monitoring, Reboot and Address bulk operations (#2124)
    [stage-1]

--- test/unit/displays/directives/dtv-display-address.tests.js
+++ test/unit/displays/directives/dtv-display-address.tests.js
@@ -1,32 +1,31 @@
 'use strict';
-xdescribe('directive: display address', function() {
+describe('directive: display address', function() {
   var COUNTRIES = ["country1","country2"]
   beforeEach(module('risevision.displays.directives'));
   beforeEach(module(function ($provide) {
-    $provide.service('displayFactory', function() {
-      return {};
-    });
     $provide.value("COUNTRIES", COUNTRIES);
     $provide.value("REGIONS_CA", [""]);
     $provide.value("REGIONS_US", [""]);
     $provide.value("TIMEZONES", [""]);
   }));
   
-  var elm, $scope, $compile;
+  var elm, $scope, $compile, display;
 
   beforeEach(inject(function($rootScope, $injector, _$compile_, $templateCache) {
+    display = {id: 123};
+    $rootScope.myDisplay = display;
     $templateCache.put('partials/displays/display-address.html', '<p>Fields</p>');
-    $scope = $rootScope.$new();
     $compile = _$compile_;
     compileDirective();
   }));
 
   function compileDirective() {
-    var tpl = '<display-address></display-address>';
-    inject(function($compile) {
-      elm = $compile(tpl)($scope);
+    var tpl = '<display-address display="myDisplay"></display-address>';
+    inject(function($compile, $rootScope) {
+      elm = $compile(tpl)($rootScope.$new());
+      $rootScope.$digest();
+      $scope = elm.isolateScope();
     });
-    $scope.$digest();
   }
 
   it('should compile html', function() {
@@ -35,6 +34,7 @@ xdescribe('directive: display address', function() {
     expect($scope.regionsCA).to.be.ok;
     expect($scope.regionsUS).to.be.ok;
     expect($scope.timezones).to.be.ok;
+    expect($scope.display).to.equal(display);
   });
 
 });
