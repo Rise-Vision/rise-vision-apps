a1dc8b5db69206c197415748b42dd10706675819
Ezequiel Conte <ezequielc@gmail.com>
2020-11-03 18:41:28 -0300

    Do not report unit test coverage on `gulp watch`. [stage-1]

--- gulpfile.js
+++ gulpfile.js
@@ -155,7 +155,7 @@ gulp.task('watch', function () {
   gulp.watch(partialsHTMLFiles, ['html2js']);
   gulp.watch(['./web/pricing-component.mjs'], ['pricing']);
   gulp.watch(['./web/tmp/partials.js', './web/scripts/**/*.js', './web/tmp/css/*.css', './web/index.html'], ['browser-sync-reload']);
-  gulp.watch(unitTestFiles, ['test:unit']);
+  gulp.watch(unitTestFiles, ['test:unit:nocoverage']);
 });
 
 
