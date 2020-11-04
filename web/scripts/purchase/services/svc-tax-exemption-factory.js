9d247e7106a57f601e9060ff94b24e2d544c9bd6
Alex Deaconu <alex.deaconu@gmail.com>
2020-11-04 10:22:42 -0500

    Submit exemption for sub-companies
    [stage-19]

--- web/scripts/purchase/services/svc-tax-exemption-factory.js
+++ web/scripts/purchase/services/svc-tax-exemption-factory.js
@@ -2,8 +2,8 @@
 
   'use strict';
   angular.module('risevision.apps.purchase')
-    .factory('taxExemptionFactory', ['storeService',
-      function (storeService) {
+    .factory('taxExemptionFactory', ['storeService', 'userState',
+      function (storeService, userState) {
         var factory = {};
 
         // Stop spinner - workaround for spinner not rendering
@@ -19,7 +19,7 @@
 
           return storeService.uploadTaxExemptionCertificate(factory.taxExemption.file)
             .then(function (blobKey) {
-              return storeService.addTaxExemption(factory.taxExemption, blobKey);
+              return storeService.addTaxExemption(userState.getSelectedCompanyId(), factory.taxExemption, blobKey);
             }).catch(function (error) {
               factory.taxExemptionError = error.message ||
                 'An error ocurred while submitting your tax exemption. Please try again.';
