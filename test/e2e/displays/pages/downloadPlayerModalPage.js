a2dce0a3b56b23ab950d4f3a8bbc6f83361640c9
Ezequiel Conte <ezequielc@gmail.com>
2020-11-09 11:49:42 -0300

    Test Rise Player download links (#2195)
    [stage-19]

--- test/e2e/displays/pages/downloadPlayerModalPage.js
+++ test/e2e/displays/pages/downloadPlayerModalPage.js
@@ -3,7 +3,12 @@ var DownloadPlayerModalPage = function() {
   var downloadPlayerModal = element(by.css('.download-player-modal'));
   var title = element(by.css('.modal-body h4'));
 
-  var downloadWindows64Button = element(by.id('downloadWindows64'));
+  var downloadWindows32Link = element(by.id('downloadWindows32'));
+  var downloadWindows64Link = element(by.id('downloadWindows64'));
+  var downloadUbuntu32Link = element(by.id('downloadUbuntu32'));
+  var downloadUbuntu64Link = element(by.id('downloadUbuntu64'));
+  var downloadRaspberryLink = element(by.id('downloadRaspberry'));
+
   var dismissButton = element(by.id('dismissButton'));
 
   this.getDownloadPlayerModal = function() {
@@ -14,8 +19,24 @@ var DownloadPlayerModalPage = function() {
     return title;
   };
 
-  this.getDownloadWindows64Button = function() {
-    return downloadWindows64Button;
+  this.getDownloadWindows32Link = function() {
+    return downloadWindows32Link;
+  };
+
+  this.getDownloadWindows64Link = function() {
+    return downloadWindows64Link;
+  };
+
+  this.getDownloadUbuntu32Link = function() {
+    return downloadUbuntu32Link;
+  };
+
+  this.getDownloadUbuntu64Link = function() {
+    return downloadUbuntu64Link;
+  };
+
+  this.getDownloadRaspberryLink = function() {
+    return downloadRaspberryLink;
   };
 
   this.getDismissButton = function() {
