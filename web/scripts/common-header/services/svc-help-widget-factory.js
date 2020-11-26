e9fa414bd714aaee23f5519f621dc8f8c01b3a11
Santiago Arriaga Noguez <santiago.noguez@risevision.com>
2020-11-26 12:12:37 -0600

    open fallback web if widget script could not load

--- web/scripts/common-header/services/svc-help-widget-factory.js
+++ web/scripts/common-header/services/svc-help-widget-factory.js
@@ -4,12 +4,15 @@
   'use strict';
 
   angular.module('risevision.common.support', [])
+    .value('HELP_URL', 'https://help.risevision.com/')
     .value('HELP_WIDGET_SCRIPT',
       '!function(e,l,v,i,o,n){e[i]||(e[i]={}),e[i].account_id=n;var g,h;g=l.createElement(v),g.type="text/javascript",g.async=1,g.src=o+n,h=l.getElementsByTagName(v)[0],h.parentNode.insertBefore(g,h);e[i].q=[];e[i].on=function(z,y){e[i].q.push([z,y])}}(window,document,"script","_elev","https://cdn.elev.io/sdk/bootloader/v4/elevio-bootloader.js?cid=","5f2331387a97f");'
     )
-    .factory('helpWidgetFactory', ['$window', 'HELP_WIDGET_SCRIPT',
-      function ($window, HELP_WIDGET_SCRIPT) {
+    .factory('helpWidgetFactory', ['$timeout', '$window', 'HELP_URL', 'HELP_WIDGET_SCRIPT',
+      function ($timeout, $window, HELP_URL, HELP_WIDGET_SCRIPT) {
         var loaded = false;
+        var widgetScriptLoaded = false;
+        var failsafeCheckDelay = 2000;
 
         function initializeWidget() {
           if (!loaded) {
@@ -18,11 +21,18 @@
 
             $window.document.body.appendChild(scriptElem);
             $window._elev.on('load', function (_elev) {
+              widgetScriptLoaded = true;
               _elev.setSettings({
                 hideLauncher: true
               });
             });
             loaded = true;
+
+            $timeout(function() {
+              if( !widgetScriptLoaded ) {
+                $window.open(HELP_URL, '_blank');
+              }
+            }, failsafeCheckDelay);
           }
         }
 
