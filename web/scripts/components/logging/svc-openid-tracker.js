4fbc0a24e09e57694696818c76d15c84d58a23a6
Santiago Arriaga Noguez <santiago.noguez@risevision.com>
2020-12-11 08:45:45 -0600

    add openid tracker

---
+++ web/scripts/components/logging/svc-openid-tracker.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('risevision.common.components.logging')
+  .factory('openidTracker', ['userState', 'analyticsFactory',
+    function (userState, analyticsFactory) {
+      return function (openidEventType, eventProperties) {
+        if (openidEventType) {
+          eventProperties = eventProperties || {};
+          angular.extend(eventProperties, {
+            openidEventType: openidEventType,
+            userId: userState.getUsername(),
+            email: userState.getUserEmail(),
+            companyId: userState.getSelectedCompanyId(),
+            companyName: userState.getSelectedCompanyName()
+          });
+          console.log(JSON.stringify(eventProperties));
+          analyticsFactory.track('OpenId Event', eventProperties);
+        }
+      };
+    }
+  ]);
