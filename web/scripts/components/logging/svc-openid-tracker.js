99b7e613d5ccb86ce337e729e5a65f43c40f27f9
Santiago Arriaga Noguez <santiago.noguez@risevision.com>
2020-12-11 10:06:25 -0600

    consider profile data [stage-9]

--- web/scripts/components/logging/svc-openid-tracker.js
+++ web/scripts/components/logging/svc-openid-tracker.js
@@ -3,13 +3,16 @@
 angular.module('risevision.common.components.logging')
   .factory('openidTracker', ['userState', 'analyticsFactory',
     function (userState, analyticsFactory) {
-      return function (openidEventType, eventProperties) {
+      return function (openidEventType, profile, eventProperties) {
         if (openidEventType) {
+          profile = profile || {};
           eventProperties = eventProperties || {};
+
           angular.extend(eventProperties, {
             openidEventType: openidEventType,
             userId: userState.getUsername(),
-            email: userState.getUserEmail(),
+            email: userState.getUserEmail() || profile.email,
+            googleUserId: profile.sub,
             companyId: userState.getSelectedCompanyId()
           });
           console.log(JSON.stringify(eventProperties));
