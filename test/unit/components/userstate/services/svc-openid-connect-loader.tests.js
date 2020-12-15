43af85be26ade5a13c4329e179ca58c4da0fc2e8
Santiago Arriaga Noguez <santiago.arriaga9@gmail.com>
2020-12-15 12:24:41 -0600

    test client returned [stage-9]

--- test/unit/components/userstate/services/svc-openid-connect-loader.tests.js
+++ test/unit/components/userstate/services/svc-openid-connect-loader.tests.js
@@ -39,4 +39,74 @@ describe("Services: openidConnectLoader", function() {
     });
   });
 
+  describe("Oidc library: ", function() {
+    var WebStorageStateStore, UserManager;
+
+    beforeEach(function() {
+      WebStorageStateStore = (function() {
+        return function() {};
+      }());
+
+      UserManager = (function() {
+        function UserManager(settings) {
+          this.settings = settings;
+        }
+
+        UserManager.prototype.signinSilent = function(params) {
+          this.params = params;
+        }
+
+        return UserManager;
+      }());
+    });
+
+    beforeEach(function() {
+      inject(function($injector) {
+        var $window = $injector.get("$window");
+        $window.Oidc = {
+          Log: {
+            WARN: 'WARN'
+          },
+          UserManager: UserManager,
+          WebStorageStateStore: WebStorageStateStore
+        };
+
+        openidConnectLoader = $injector.get("openidConnectLoader");
+      });
+    });
+
+    it("should return client", function(done) {
+      openidConnectLoader().then( function(client) {
+        expect(client).to.be.ok;
+        expect(client.settings).to.be.ok;
+        expect(client.settings.authority).to.equal('https://accounts.google.com/');
+
+        expect(client.signinSilent).to.be.ok;
+        expect(client.signinSilent).to.be.a("function");
+
+        done();
+      });
+    });
+
+    it("should pass through params to signinSilent", function(done) {
+      openidConnectLoader().then( function(client) {
+        client.signinSilent({ param: 'param' });
+
+        expect(client.params).to.deep.equal({ param: 'param' });
+
+        done();
+      });
+    });
+
+    it("should use params with login_hint if no params were provided", function(done) {
+      openidConnectLoader().then( function(client) {
+        client.signinSilent();
+
+        expect(client.params).to.deep.equal({ login_hint: 'a username' });
+
+        done();
+      });
+    });
+  });
+
 });
