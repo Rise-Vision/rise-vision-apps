7666bd6f7007e0bfe4b548121aa418a90e6eeba0
Alex Deaconu <alex.deaconu@gmail.com>
2020-12-16 10:59:37 -0500

    Remove 3rd party cookie check
    Revert core version url
    
    [stage-19]

--- web/scripts/common-header/services/svc-cookie-tester.js
+++ web/scripts/common-header/services/svc-cookie-tester.js
@@ -1,53 +1,20 @@
 'use strict';
-angular.module('risevision.common.cookie', ['risevision.common.config'])
-  .service('cookieTester', ['$q', '$document', '$http', 'COOKIE_CHECK_URL',
-    function ($q, $document, $http, COOKIE_CHECK_URL) {
+angular.module('risevision.common.cookie', [])
+  .service('cookieTester', ['$q', '$document',
+    function ($q, $document) {
       var svc = {};
 
-      svc.checkCookies = function () {
-        var deferred = $q.defer();
-        $q.all([svc.checkLocalCookiePermission(), svc.checkThirdPartyCookiePermission()])
-          .then(function () {
-            deferred.resolve();
-          }, function () {
-            deferred.reject();
-          });
-        return deferred.promise;
-      };
-
-      svc.checkLocalCookiePermission = function () {
+      var _checkLocalCookiePermission = function () {
         $document[0].cookie = 'rv-test-local-cookie=yes';
         if ($document[0].cookie.indexOf('rv-test-local-cookie') > -1) {
-          return $q.when(true);
+          return $q.resolve();
         }
 
-        return $q.reject(false);
+        return $q.reject();
       };
 
-      svc.checkThirdPartyCookiePermission = function () {
-        var deferred = $q.defer();
-
-        $http.get(COOKIE_CHECK_URL + '/createThirdPartyCookie', {
-            withCredentials: true
-          })
-          .then(function () {
-            return $http.get(COOKIE_CHECK_URL + '/checkThirdPartyCookie', {
-              withCredentials: true
-            });
-          })
-          .then(function (resp) {
-            if (resp.data.check === 'true') {
-              deferred.resolve(true);
-            } else {
-              deferred.reject(false);
-            }
-          })
-          .then(null, function () {
-            // Resolve on API failures
-            deferred.resolve(false);
-          });
-
-        return deferred.promise;
+      svc.checkCookies = function () {
+        return _checkLocalCookiePermission();
       };
 
       return svc;
