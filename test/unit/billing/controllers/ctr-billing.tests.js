de25adaa5c12ae9bba5a08eee58b46da4b289911
Alex Deaconu <alex.deaconu@gmail.com>
2021-01-13 09:44:35 -0500

    Clean up unimplemented features and show legacy actions
    Hide credit cards and show edit payment methods button
    Hide subscription actions and show edit subscription
    Hide yearly and early invoice features
    Hide tax exemption button and show no tax exemption message
    
    [stage-19]

--- test/unit/billing/controllers/ctr-billing.tests.js
+++ test/unit/billing/controllers/ctr-billing.tests.js
@@ -1,7 +1,7 @@
 'use strict';
 describe('controller: BillingCtrl', function () {
   var sandbox = sinon.sandbox.create();
-  var $rootScope, $scope, $loading, $timeout, ScrollingListService, listServiceInstance;
+  var $rootScope, $scope, $loading, ScrollingListService, listServiceInstance;
 
   beforeEach(module('risevision.apps.billing.controllers'));
 
@@ -33,14 +33,6 @@ describe('controller: BillingCtrl', function () {
     $provide.service('currentPlanFactory', function() {
       return {};
     });
-    $provide.service('ChargebeeFactory', function () {
-      return function() {
-        return {
-          openPaymentSources: sandbox.stub(),
-          openSubscriptionDetails: sandbox.stub()
-        };
-      };
-    });
     $provide.service('billing', function () {
       return {
         getSubscriptions: 'getSubscriptions',
@@ -59,7 +51,6 @@ describe('controller: BillingCtrl', function () {
     $rootScope = _$rootScope_;
     $scope = $rootScope.$new();
     $loading = $injector.get('$loading');
-    $timeout = $injector.get('$timeout');
     ScrollingListService = $injector.get('ScrollingListService');
 
     $controller('BillingCtrl', {
@@ -75,8 +66,6 @@ describe('controller: BillingCtrl', function () {
   it('should exist',function () {
     expect($scope).to.be.ok;
     expect($scope.companySettingsFactory).to.be.ok;
-    expect($scope.editPaymentMethods).to.be.a.function;
-    expect($scope.editSubscription).to.be.a.function;
 
     expect($scope.subscriptions).to.be.ok;
     expect($scope.invoices).to.be.ok;
@@ -108,38 +97,12 @@ describe('controller: BillingCtrl', function () {
     });
   });
 
-  describe('payment methods', function () {
-    it('should show Chargebee payment methods', function () {
-      $scope.editPaymentMethods();
-      expect($scope.chargebeeFactory.openPaymentSources).to.be.calledOnce;
-      expect($scope.chargebeeFactory.openPaymentSources.getCall(0).args[0]).to.equal('testId');
-    });
-  });
-
-  describe('edit subscriptions', function () {
-    it('should show Chargebee subscription details', function () {
-      $scope.editSubscription({ id: 'subs1' });
-      expect($scope.chargebeeFactory.openSubscriptionDetails).to.be.calledOnce;
-      expect($scope.chargebeeFactory.openSubscriptionDetails.getCall(0).args[0]).to.equal('testId');
-      expect($scope.chargebeeFactory.openSubscriptionDetails.getCall(0).args[1]).to.equal('subs1');
-    });
-  });
-
   describe('events: ', function () {
-    it('should reload Subscriptions when Subscription is updated on Customer Portal', function () {
-      $rootScope.$emit('chargebee.subscriptionChanged');
-      $timeout.flush();
-
-      expect($loading.start).to.be.calledOnce;
-      expect(listServiceInstance.doSearch).to.be.calledOnce;
-    });
-
     it('should reload Subscriptions when Subscription is started', function () {
       $rootScope.$emit('risevision.company.planStarted');
 
       expect(listServiceInstance.doSearch).to.be.calledOnce;
     });
-
   });
 
   describe('data formatting', function () {
--- web/scripts/billing/controllers/ctr-subscription.js
+++ web/scripts/billing/controllers/ctr-subscription.js
@@ -1,13 +1,14 @@
 'use strict';
 
 angular.module('risevision.apps.billing.controllers')
-  .controller('SubscriptionCtrl', ['$scope', '$loading', 'subscriptionFactory', 'userState',
-  'creditCardFactory', 'companySettingsFactory',
-    function ($scope, $loading, subscriptionFactory, userState, creditCardFactory,
-      companySettingsFactory) {
+  .controller('SubscriptionCtrl', ['$scope', '$rootScope', '$loading', '$timeout', 'subscriptionFactory',
+  'userState', 'creditCardFactory', 'companySettingsFactory', 'ChargebeeFactory',
+    function ($scope, $rootScope, $loading, $timeout, subscriptionFactory, userState, creditCardFactory,
+      companySettingsFactory, ChargebeeFactory) {
       $scope.subscriptionFactory = subscriptionFactory;
       $scope.creditCardFactory = creditCardFactory;
       $scope.companySettingsFactory = companySettingsFactory;
+      $scope.chargebeeFactory = new ChargebeeFactory();
       $scope.company = userState.getCopyOfSelectedCompany();
 
       $scope.$watch('subscriptionFactory.loading', function (newValue) {
@@ -20,6 +21,27 @@ angular.module('risevision.apps.billing.controllers')
 
       $scope.isInvoiced = function() {
         return subscriptionFactory.item && !subscriptionFactory.item.card;
+      };
+
+      var _reloadSubscription = function () {
+        $loading.start('subscription-loader');
+
+        $timeout(function () {
+          subscriptionFactory.reloadSubscription();
+        }, 10000);
+      };
+
+      $rootScope.$on('chargebee.subscriptionChanged', _reloadSubscription);
+      $rootScope.$on('chargebee.subscriptionCancelled', _reloadSubscription);
+
+      $scope.editSubscription = function (subscription) {
+        var subscriptionId = subscription.id;
+
+        $scope.chargebeeFactory.openSubscriptionDetails(userState.getSelectedCompanyId(), subscriptionId);
+      };
+
+      $scope.editPaymentMethods = function () {
+        $scope.chargebeeFactory.openPaymentSources(userState.getSelectedCompanyId());
       };
 
     }
