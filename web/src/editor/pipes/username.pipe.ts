7c085c88a3f0ea1a144a1d8f603724c2c0bfd84e
Ezequiel Conte <ezequielc@gmail.com>
2021-02-09 17:04:26 -0300

    Refactoring [stage-1]

--- web/src/editor/pipes/username.pipe.ts
+++ web/src/editor/pipes/username.pipe.ts
@@ -3,7 +3,7 @@ import { Pipe, PipeTransform } from '@angular/core';
 @Pipe({name: 'username'})
 export class UsernamePipe implements PipeTransform {
   transform(email: string) {
-    var username = email;
+    var username = email || 'N/A';
     if (email && email.indexOf('@') !== -1) {
         username = email.substring(0, email.indexOf('@'));
     }
