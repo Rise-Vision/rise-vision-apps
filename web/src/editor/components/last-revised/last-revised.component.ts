564c9d0b1f48b2ef42fee03a93d9e9007636593b
Ezequiel Conte <ezequielc@gmail.com>
2021-02-09 17:17:18 -0300

    Use inline template until build is fixed [stage-1]

--- web/src/editor/components/last-revised/last-revised.component.ts
+++ web/src/editor/components/last-revised/last-revised.component.ts
@@ -4,7 +4,17 @@ declare var angular: angular.IAngularStatic;
  
  @Component({
     selector: 'last-revised',
-    templateUrl: 'src/editor/components/last-revised/last-revised.component.html'
+    // templateUrl: 'src/editor/components/last-revised/last-revised.component.html'
+    template: `
+      <span class="text-muted">
+        <small>
+          <span>
+            {{revisionStatusName}}
+          </span> 
+          {{changeDate | date:'d-MMM-yyyy h:mm a'}} by {{changedBy | username}}
+        </small>
+      </span>
+    `
  })
  
  export class LastRevisedComponent {
