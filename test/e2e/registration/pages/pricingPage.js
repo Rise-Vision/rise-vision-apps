c2ac9f4ee8957eaa715acaa63f4ffe7143efd871
Ezequiel Conte <ezequielc@gmail.com>
2021-03-29 14:17:14 -0300

    Change 'Send me a Quote' to 'Contact Sales' and update link (#2487)
    Fix #2472
    [stage-1]

--- test/e2e/registration/pages/pricingPage.js
+++ test/e2e/registration/pages/pricingPage.js
@@ -1,9 +1,14 @@
 'use strict';
 var PricingPage = function() {
   var subscribeButton = element(by.id('subscribeButton'));
+  var contactSalesButton = element(by.id('contactSalesButton'));
 
   this.getSubscribeButton = function() {
     return subscribeButton;
+  };
+
+  this.getContactSalesButton = function() {
+    return contactSalesButton;
   };
 };
 
