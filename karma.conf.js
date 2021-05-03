2c255f7391a6b9067ec7a42afbc2aea6e69ca177
Alex Deaconu <alex.deaconu@gmail.com>
2021-05-03 15:36:15 -0400

    Fix coverage folder for angular2
    [stage-11]

--- karma.conf.js
+++ karma.conf.js
@@ -4,7 +4,10 @@
 module.exports = function (config) {
   config.set({
     basePath: '',
-    frameworks: ['jasmine', '@angular-devkit/build-angular'],
+    frameworks: [
+      'jasmine',
+      '@angular-devkit/build-angular'
+    ],
     plugins: [
       require('karma-jasmine'),
       require('karma-chrome-launcher'),
@@ -16,7 +19,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, './coverage/rise-vision-apps-angular2'),
+      dir: require('path').join(__dirname, './reports/coverage/angular2'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
