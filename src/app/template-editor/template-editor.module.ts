83c5601688e2f33a343666c0951ef1e0ef6fd1d0
Ezequiel Conte <ezequielc@gmail.com>
2021-05-12 16:39:41 -0300

    Create Template Editor module in Angular [stage-1]

--- src/app/template-editor/template-editor.module.ts
+++ src/app/template-editor/template-editor.module.ts
@@ -1,19 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { LastRevisedComponent } from './components/last-revised/last-revised.component';
-import { UsernamePipe } from './pipes/username.pipe';
+import { CanvaButtonComponent } from './components/canva-button/canva-button.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    LastRevisedComponent, 
-    UsernamePipe
+    CanvaButtonComponent
   ]
 })
-export class EditorModule {
+export class TemplateEditorModule {
   //workaround for including downgraded components into build files
   //https://github.com/angular/angular/issues/35314#issuecomment-584821399
-  static entryComponents = [ LastRevisedComponent ]
+  static entryComponents = [ CanvaButtonComponent ]
 }
