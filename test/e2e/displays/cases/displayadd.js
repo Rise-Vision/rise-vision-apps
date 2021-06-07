b36cb2c71ea6bb1103642618768fcc66374695fe
Ezequiel Conte <ezequielc@gmail.com>
2021-06-07 13:11:19 -0300

    Web Player install links (#2639)
    * Web Player install links
    [stage-1]
    
    * Fix e2e tests
    [stage-1]
    
    * Update and show article for WebPlayer install
    [stage-1]
    
    * Link to articles for installing Players (#2640)
    
    [stage-1]

--- test/e2e/displays/cases/displayadd.js
+++ test/e2e/displays/cases/displayadd.js
@@ -1,7 +1,5 @@
 'use strict';
 var expect = require('rv-common-e2e').expect;
-var url = require('url');
-var https = require('https');
 var HomePage = require('./../../common/pages/homepage.js');
 var SignInPage = require('./../../common/pages/signInPage.js');
 var CommonHeaderPage = require('./../../common-header/pages/commonHeaderPage.js');
@@ -96,44 +94,9 @@ var DisplayAddScenarios = function() {
         
         expect(downloadPlayerModalPage.getDownloadPlayerModal().isDisplayed()).to.eventually.be.true;
 
-        expect(downloadPlayerModalPage.getTitle().getText()).to.eventually.equal('Install Rise Player');
+        expect(downloadPlayerModalPage.getTitle().getText()).to.eventually.equal('Install Rise Player on:');
         
-        expect(downloadPlayerModalPage.getDownloadWindows32Link().isDisplayed()).to.eventually.be.true;
-      });
-
-      it('should provide HTTPS download links to prevent browsers block',function() {
-        expect(downloadPlayerModalPage.getDownloadWindows32Link().getAttribute('href')).to.eventually.match(/^https:/);
-        expect(downloadPlayerModalPage.getDownloadWindows64Link().getAttribute('href')).to.eventually.match(/^https:/);
-
-        expect(downloadPlayerModalPage.getDownloadUbuntu32Link().getAttribute('href')).to.eventually.match(/^https:/);
-        expect(downloadPlayerModalPage.getDownloadUbuntu64Link().getAttribute('href')).to.eventually.match(/^https:/);
-
-        expect(downloadPlayerModalPage.getDownloadRaspberryLink().getAttribute('href')).to.eventually.match(/^https:/);
-      });
-
-      it('should provide working download links',function() {
-        downloadPlayerModalPage.getDownloadWindows32Link().getAttribute('href').then(function(href) {
-          var httpHeadRequest = function() {
-            var defer = protractor.promise.defer();
-            const options = {
-              hostname: url.parse(href).hostname,
-              port: 443,
-              path: url.parse(href).path,
-              method: 'HEAD',
-            }
-            https.request(options, function(response) {
-              defer.fulfill(response.statusCode);
-            }).on('error', function(e) {
-              defer.reject('Request failed: ' + e.message);
-            }).end();
-            return defer.promise;
-          };
-
-          protractor.promise.controlFlow().execute(httpHeadRequest).then(function (statusCode) {
-              expect(statusCode).to.equal(200);
-          });
-        });
-
+        expect(downloadPlayerModalPage.getInstallWindowsLink().isDisplayed()).to.eventually.be.true;
       });
 
       it('should close modal',function() {
--- test/e2e/displays/pages/downloadPlayerModalPage.js
+++ test/e2e/displays/pages/downloadPlayerModalPage.js
@@ -3,11 +3,7 @@ var DownloadPlayerModalPage = function() {
   var downloadPlayerModal = element(by.css('.download-player-modal'));
   var title = element(by.css('.modal-body h4'));
 
-  var downloadWindows32Link = element(by.id('downloadWindows32'));
-  var downloadWindows64Link = element(by.id('downloadWindows64'));
-  var downloadUbuntu32Link = element(by.id('downloadUbuntu32'));
-  var downloadUbuntu64Link = element(by.id('downloadUbuntu64'));
-  var downloadRaspberryLink = element(by.id('downloadRaspberry'));
+  var installWindowsLink = element(by.id('installWindows'));
 
   var dismissButton = element(by.id('dismissButton'));
 
@@ -19,24 +15,8 @@ var DownloadPlayerModalPage = function() {
     return title;
   };
 
-  this.getDownloadWindows32Link = function() {
-    return downloadWindows32Link;
-  };
-
-  this.getDownloadWindows64Link = function() {
-    return downloadWindows64Link;
-  };
-
-  this.getDownloadUbuntu32Link = function() {
-    return downloadUbuntu32Link;
-  };
-
-  this.getDownloadUbuntu64Link = function() {
-    return downloadUbuntu64Link;
-  };
-
-  this.getDownloadRaspberryLink = function() {
-    return downloadRaspberryLink;
+  this.getInstallWindowsLink = function() {
+    return installWindowsLink;
   };
 
   this.getDismissButton = function() {
