836907af52c22c830ea9811811b5f317335f7160
Alex Deaconu <alex.deaconu@gmail.com>
2021-06-16 10:08:21 -0400

    Add web and android player checks (#2665)
    Hide unavailable functionality for the players
    Hide displayfields when not available
    
    Remove unused functions

--- src/scripts/displays/services/svc-player-pro-factory.js
+++ src/scripts/displays/services/svc-player-pro-factory.js
@@ -3,34 +3,19 @@
 angular.module('risevision.displays.services')
   .value('PLAYER_VERSION_DATE_REGEX', /\d{4}\.\d{2}\.\d{2}\.*/)
   .value('SCREENSHOT_PLAYER_VERSION', '2017.01.10.17.33')
-  .value('OFFLINE_PLAY_PLAYER_VERSION', '2017.07.31.15.31')
   .value('DISPLAY_CONTROL_PLAYER_VERSION', '2018.01.15.16.31')
   .value('CHROMEOS_PLAYER_VERSION', '2018.07.20.10229')
   .value('CHROMEOS_SCREENSHOT_PLAYER_VERSION', '2018.08.17.8388')
   .factory('playerProFactory', ['userState', 'PLAYER_VERSION_DATE_REGEX',
-    'SCREENSHOT_PLAYER_VERSION', 'OFFLINE_PLAY_PLAYER_VERSION', 'DISPLAY_CONTROL_PLAYER_VERSION',
+    'SCREENSHOT_PLAYER_VERSION', 'DISPLAY_CONTROL_PLAYER_VERSION',
     'CHROMEOS_PLAYER_VERSION', 'CHROMEOS_SCREENSHOT_PLAYER_VERSION',
     function (userState, PLAYER_VERSION_DATE_REGEX,
-      SCREENSHOT_PLAYER_VERSION, OFFLINE_PLAY_PLAYER_VERSION, DISPLAY_CONTROL_PLAYER_VERSION,
+      SCREENSHOT_PLAYER_VERSION, DISPLAY_CONTROL_PLAYER_VERSION,
       CHROMEOS_PLAYER_VERSION, CHROMEOS_SCREENSHOT_PLAYER_VERSION) {
       var factory = {};
 
       var _compareVersion = function (minimumVersion, currentVersion) {
         return PLAYER_VERSION_DATE_REGEX.test(currentVersion) && currentVersion >= minimumVersion;
-      };
-
-      factory.is3rdPartyPlayer = function (display) {
-        display = display || {};
-        var playerName = (display.playerName || '').toLowerCase();
-        var playerVersion = (display.playerVersion || '').toLowerCase();
-        var os = (display.os || '').toLowerCase();
-        var isCAP = playerName === 'riseplayerpackagedapp';
-        var isRisePlayer = playerName.indexOf('riseplayer') !== -1;
-        var isCenique = (playerName + playerVersion).indexOf('cenique') !== -1;
-        var isAndroid = os.indexOf('android') !== -1;
-        var isCROSLegacy = factory.isCROSLegacy(display);
-
-        return !!playerName && (isCAP || isCROSLegacy || isAndroid || isCenique || !isRisePlayer);
       };
 
       factory.isCROSLegacy = function (display) {
@@ -49,10 +34,14 @@ angular.module('risevision.displays.services')
           !factory.isElectronPlayer(display) && _compareVersion(CHROMEOS_PLAYER_VERSION, display.playerVersion));
       };
 
-      factory.isUnsupportedPlayer = function (display) {
-        var newDisplay = !(display && display.playerName && display.playerVersion);
-        return !!(!newDisplay && !factory.isElectronPlayer(display) &&
-          !factory.isChromeOSPlayer(display)) || factory.is3rdPartyPlayer(display);
+      factory.isWebPlayer = function (display) {
+        return !!(display && display.playerName &&
+          display.playerName.indexOf('Web Player') !== -1);
+      };
+
+      factory.isAndroidPlayer = function (display) {
+        return !!(display && display.playerName &&
+          display.playerName.indexOf('Android Player') !== -1);
       };
 
       factory.isScreenshotCompatiblePlayer = function (display) {
@@ -62,11 +51,6 @@ angular.module('risevision.displays.services')
           CHROMEOS_SCREENSHOT_PLAYER_VERSION, display.playerVersion);
 
         return electronSupported || chromeOSSupported;
-      };
-
-      factory.isOfflinePlayCompatiblePayer = function (display) {
-        return !!(display && factory.isElectronPlayer(display) &&
-          _compareVersion(OFFLINE_PLAY_PLAYER_VERSION, display.playerVersion));
       };
 
       factory.isDisplayControlCompatiblePlayer = function (display) {
