318f7b875fe46a7f02f8bb518e5fba754f9fbe9f
Ezequiel Conte <ezequielc@gmail.com>
2021-07-02 16:42:29 -0300

    Migrate streamline-icon [stage-13]

--- src/app/template-editor/template-editor.module.ts
+++ src/app/template-editor/template-editor.module.ts
@@ -3,18 +3,20 @@ import { CommonModule } from '@angular/common';
 import { CanvaButtonComponent } from './components/canva-button/canva-button.component';
 import { AttributeDataService } from './services/attribute-data.service';
 import { BlueprintService } from './services/blueprint.service';
+import { StreamlineIconComponent } from './components/streamline-icon/streamline-icon.component';
 
 @NgModule({
   imports: [
     CommonModule
   ],
   declarations: [
-    CanvaButtonComponent
+    CanvaButtonComponent,
+    StreamlineIconComponent
   ]
 })
 export class TemplateEditorModule {
   //workaround for including downgraded components into build files
   //https://github.com/angular/angular/issues/35314#issuecomment-584821399
-  static entryComponents = [ CanvaButtonComponent ]
+  static entryComponents = [ CanvaButtonComponent, StreamlineIconComponent ]
   static providers = [ AttributeDataService, BlueprintService ]
 }
