89c2123bf5daa325b49dda1621ab5780dca6e085
Ezequiel Conte <ezequielc@gmail.com>
2021-07-08 11:39:41 -0300

    Common header height directive migration (#2727)
    * common header height directive migration
    [stage-1]

--- src/test.ts
+++ src/test.ts
@@ -28,8 +28,10 @@ declare global {
 export {};
 
 angular.module('risevision.apps.services',[])
+angular.module('risevision.common.header.directives',[])
 angular.module('risevision.editor.directives',[])
 angular.module('risevision.apps.purchase',[])
+angular.module('risevision.template-editor.controllers',[])
 angular.module('risevision.template-editor.directives',[])
 angular.module('risevision.template-editor.controllers',[])
 angular.module('risevision.template-editor.services',[])
