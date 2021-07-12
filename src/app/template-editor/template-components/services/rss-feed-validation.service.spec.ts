d42090abf57e547697e0de70cf71fe933f5b99e3
Ezequiel Conte <ezequielc@gmail.com>
2021-07-12 18:09:44 -0300

    rss unit tests [stage-1]

--- src/app/template-editor/template-components/services/rss-feed-validation.service.spec.ts
+++ src/app/template-editor/template-components/services/rss-feed-validation.service.spec.ts
@@ -1,17 +1,123 @@
-import { expect } from 'chai';
+import { assert, expect } from 'chai';
 import { TestBed } from '@angular/core/testing';
-
 import { RssFeedValidationService } from './rss-feed-validation.service';
+import { HttpTestingController, HttpClientTestingModule } from '@angular/common/http/testing';
+import { PromiseUtilsService } from 'src/app/shared/services/promise-utils.service';
 
 describe('RssFeedValidationService', () => {
-  let service: RssFeedValidationService;
+  let rssFeedValidation: RssFeedValidationService;
+
+  let $httpBackend: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
-    service = TestBed.inject(RssFeedValidationService);
+    TestBed.configureTestingModule({
+      imports: [
+        HttpClientTestingModule
+      ],
+      providers: [
+        {provide: PromiseUtilsService, useValue: new PromiseUtilsService()}        
+      ]
+    });
+
+    $httpBackend = TestBed.inject(HttpTestingController);
+    rssFeedValidation = TestBed.inject(RssFeedValidationService);
   });
 
   it('should be created', () => {
-    expect(service).to.exist;
+    expect(rssFeedValidation).to.exist;
+    expect(rssFeedValidation.isParsable).to.be.a('function');
+    expect(rssFeedValidation.isValid).to.be.a('function');
   });
+
+  describe('isParsable', function() {
+
+    it("should return '' if url is empty", function( done ) {
+      var url = "";
+
+      rssFeedValidation.isParsable(url)
+        .then(function (result) {
+          expect(result).to.be.equal('');
+
+          done();
+        })
+        .catch(function(err) {
+          assert.fail('Unexpected ' + err);
+        });
+    });
+
+    it("should return 'VALID' if response does not contain error", function( done ) {
+      var url = "http://rss.cnn.com/rss/cnn_topstories.rss";
+      rssFeedValidation.isParsable(url)
+        .then(function (result) {
+          expect(result).to.be.equal('VALID');
+
+          done();
+        })
+        .catch(function(err) {
+          assert.fail('Unexpected ' + err);
+        });
+      $httpBackend.expectOne('https://feed-parser.risevision.com/' + url)
+        .flush([{"test":"rss"}]);
+    });
+
+    it("should return 'UNAUTHORIZED' if feed requires authentication", function( done ) {
+      var url = "http://feeds.reuters.com/reuters/topNews";
+      rssFeedValidation.isParsable(url)
+        .then(function (result) {
+          expect(result).to.be.equal('UNAUTHORIZED');
+
+          done();
+        })
+        .catch(function(err) {
+          assert.fail('Unexpected ' + err);
+        });
+      $httpBackend.expectOne('https://feed-parser.risevision.com/' + url)
+      .flush({ Error: '401 Unauthorized' });
+    });
+
+    it("should return 'NON_FEED' if url provided is not a RSS feed", function( done ) {
+      var url = "http://tsn.ca";
+      rssFeedValidation.isParsable(url)
+        .then(function (result) {
+          expect(result).to.be.equal('NON_FEED');
+
+          done();
+        })
+        .catch(function(err) {
+          assert.fail('Unexpected ' + err);
+        });
+      $httpBackend.expectOne('https://feed-parser.risevision.com/' + url)
+      .flush({ Error: 'Not a feed' });
+    });
+
+    it("should return 'NOT_FOUND' if url provided does not have a recognizable domain", function( done ) {
+      var url = "http://ffasfsaa.com";
+      rssFeedValidation.isParsable(url)
+        .then(function (result) {
+          expect(result).to.be.equal('NOT_FOUND');
+
+          done();
+        })
+        .catch(function(err) {
+          assert.fail('Unexpected ' + err);
+        });      
+      $httpBackend.expectOne('https://feed-parser.risevision.com/' + url)
+      .flush({ Error: 'getaddrinfo ENOTFOUND safasfsafsa.com safasfsafsa.com:80' });
+    });
+
+    it("should return 'VALID' if response has error not pertaining to the feed", function( done ) {
+      var url = "http://rss.cnn.com/rss/cnn_topstories.rss";
+      rssFeedValidation.isParsable(url)
+        .then(function (result) {
+          expect(result).to.be.equal('VALID');
+
+          done();
+        })
+        .catch(function(err) {
+          assert.fail('Unexpected ' + err);
+        });    
+      $httpBackend.expectOne('https://feed-parser.risevision.com/' + url)
+      .flush({ Error: 'ETIMEDOUT' });
+    });
+  } );
 });
